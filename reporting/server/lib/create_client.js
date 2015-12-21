199700985e523eef77cb23205af74c893e352c7a
Joe Fleming <joe.fleming@gmail.com>
2015-12-21 16:53:17 -0700

    disable auth without username and password
    Original commit: elastic/x-pack@8e6e17e2252b88bf4ef9bd9563b763b4b2aa3c57

--- reporting/server/lib/create_client.js
+++ reporting/server/lib/create_client.js
@@ -1,6 +1,11 @@
 module.exports = function (elasticsearch, config) {
   var username = config.get('reporting.auth.username');
   var password = config.get('reporting.auth.password');
-  const auth = { username, password };
-  return elasticsearch.createClient(auth);
+  let opts = { auth: false };
+
+  if (username || password) {
+    opts = { username, password };
+  }
+
+  return elasticsearch.createClient(opts);
 };
