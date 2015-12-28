4493f372808a1965201a69af9416b3667cf5f43d
Joe Fleming <joe.fleming@gmail.com>
2015-12-28 12:20:25 -0700

    replace get_auth_header
    Original commit: elastic/x-pack@f427389785c68d85c092fa9901b6b43ef3d6dbae

--- shield/server/lib/get_auth_header.js
+++
@@ -1,4 +0,0 @@
-module.exports = function getAuthHeader(username, password) {
-  const auth = new Buffer(`${username}:${password}`).toString('base64');
-  return {authorization: `Basic ${auth}`};
-};
