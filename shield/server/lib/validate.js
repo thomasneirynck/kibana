cf6c7acaeeddd2c56718e581887942d32703c66c
Lukas Olson <olson.lukas@gmail.com>
2016-01-20 12:15:00 -0700

    Update login to use authenticate api
    Original commit: elastic/x-pack@364f734a1ea1cd6ee7b43dda4e9649a3dc0654a4

--- shield/server/lib/validate.js
+++ shield/server/lib/validate.js
@@ -1,6 +1,4 @@
-const _ = require('lodash');
 const root = require('requirefrom')('');
-const basicAuth = root('server/lib/basic_auth');
 
 module.exports = (server) => {
   const isValidUser = root('server/lib/is_valid_user')(server);
@@ -8,11 +6,9 @@ module.exports = (server) => {
   return function validate(request, session, callback) {
     const {username, password} = session;
 
-    return isValidUser(username, password).then(() => {
-      _.assign(request.headers, basicAuth.getHeader(username, password));
-      return callback(null, true);
-    }, (error) => {
-      return callback(error, false);
-    });
+    return isValidUser(request, username, password).then(
+      () => callback(null, true),
+      (error) => callback(error, false)
+    );
   };
 };
