44b5b09279579fc6216875e459dca39f3463f035
Lukas Olson <olson.lukas@gmail.com>
2016-01-30 16:54:03 -0700

    Kibana/Shield: Implement role APIs
    Original commit: elastic/x-pack@48c014a01560960d643e73f2e63b6a26e5656bfd

--- shield/server/routes/api/v1/roles.js
+++ shield/server/routes/api/v1/roles.js
@@ -1,6 +1,8 @@
-const Boom = require('boom');
+const {flow} = require('lodash');
 const root = require('requirefrom')('');
 const getClient = root('server/lib/get_client_shield');
+const roleSchema = root('server/lib/role_schema');
+const wrapError = root('server/lib/wrap_error');
 
 module.exports = (server) => {
   const callWithRequest = getClient(server).callWithRequest;
@@ -9,31 +11,40 @@ module.exports = (server) => {
     method: 'GET',
     path: '/api/shield/v1/roles',
     handler(request, reply) {
-
+      return callWithRequest(request, 'shield.getRole').then(reply, flow(wrapError, reply));
     }
   });
 
   server.route({
     method: 'GET',
-    path: '/api/shield/v1/roles/{roleId}',
+    path: '/api/shield/v1/roles/{rolename}',
     handler(request, reply) {
-      const roleId = request.params.roleId;
+      const rolename = request.params.rolename;
+      return callWithRequest(request, 'shield.getRole', {rolename}).then(reply, flow(wrapError, reply));
     }
   });
 
   server.route({
     method: 'PUT',
-    path: '/api/shield/v1/roles/{roleId}',
+    path: '/api/shield/v1/roles/{rolename}',
     handler(request, reply) {
-      const roleId = request.params.roleId;
+      const rolename = request.params.rolename;
+      const body = request.payload;
+      return callWithRequest(request, 'shield.putRole', {rolename, body}).then(reply, flow(wrapError, reply));
+    },
+    config: {
+      validate: {
+        payload: roleSchema
+      }
     }
   });
 
   server.route({
     method: 'DELETE',
-    path: '/api/shield/v1/roles/{roleId}',
+    path: '/api/shield/v1/roles/{rolename}',
     handler(request, reply) {
-      const roleId = request.params.roleId;
+      const rolename = request.params.rolename;
+      return callWithRequest(request, 'shield.deleteRole', {rolename}).then(reply, flow(wrapError, reply));
     }
   });
 };
