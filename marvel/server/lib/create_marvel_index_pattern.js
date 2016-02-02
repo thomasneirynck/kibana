ff629302acda0924f1d4cf149e09fba921c3e275
Timothy Sullivan <tsullivan@elastic.co>
2016-02-02 13:28:16 -0700

    kibana marvel - don’t update Kibana default index pattern
    Original commit: elastic/x-pack@c9097cb93efd51c95f8efbf9a5a5b9befcec9e19

--- marvel/server/lib/create_marvel_index_pattern.js
+++ marvel/server/lib/create_marvel_index_pattern.js
@@ -12,38 +12,15 @@ module.exports = function (server) {
     type: type,
     id: id,
     ignoreUnavailable: true
-  }).catch(function (resp) {
+  })
+  .catch(function (resp) {
     if (resp.status !== 404) return Promise.reject(resp);
+    // Create an index pattern for Marvel indices.
     return client.index({
       index: index,
       type: type,
       id: id,
       body: marvelIndexPattern
-    })
-    .then(function () {
-      return client.search({
-        index: index,
-        type: 'config',
-        body: {
-          sort: { 'buildNum': { order: 'desc' } },
-          size: 1
-        }
-      });
-    })
-    .then(function (resp) {
-      var config;
-      if (resp.hits.total) {
-        config = resp.hits.hits[0];
-        if (!config._source.defaultIndex) {
-          config._source.defaultIndex = id;
-          return client.index({
-            index: config._index,
-            type: config._type,
-            id: config._id,
-            body: config._source
-          });
-        }
-      }
     });
   });
 };
