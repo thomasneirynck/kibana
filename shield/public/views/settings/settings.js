c7b65f4374ceb347e5f5e71ff9fd3a65882863ce
Lukas Olson <olson.lukas@gmail.com>
2016-02-05 22:06:03 -0700

    Kibana/Shield: User list UI
    Original commit: elastic/x-pack@fbda347c1ff91e9e95d7322c4730ab844d7f39eb

--- shield/public/views/settings/settings.js
+++ shield/public/views/settings/settings.js
@@ -2,10 +2,55 @@ import {constant} from 'lodash';
 import registry from 'ui/registry/settings_sections';
 import routes from 'ui/routes';
 import template from 'plugins/shield/views/settings/settings.html';
+import 'angular-resource';
+import 'plugins/shield/services/users';
 
 routes.when('/settings/security', {
   template,
-  controller: function ($scope) {}
+  resolve: {
+    users(shieldUsers) {
+      return shieldUsers.query();
+    }
+  },
+  controller($scope, $route, $q, shieldUsers) {
+    $scope.users = $route.current.locals.users;
+    $scope.selectedUsers = [];
+
+    $scope.deleteUsers = () => {
+      if (confirm('Are you sure you want to delete the selected users? This action is irreversible!')) {
+        $q.all($scope.selectedUsers.map((user) => user.$delete()))
+        .then(() => {
+          $scope.selectedUsers.map((user) => {
+            const i = $scope.users.indexOf(user);
+            $scope.users.splice(i, 1);
+          });
+
+          $scope.selectedUsers.length = 0;
+        });
+      }
+    };
+
+    $scope.toggleAll = () => {
+      if ($scope.allSelected()) {
+        $scope.selectedUsers.length = 0;
+      } else {
+        $scope.selectedUsers = $scope.users.slice();
+      }
+    };
+
+    $scope.allSelected = () => $scope.users.length && $scope.users.length === $scope.selectedUsers.length;
+
+    $scope.toggleSelected = (user) => {
+      const i = $scope.selectedUsers.indexOf(user);
+      if (i >= 0) {
+        $scope.selectedUsers.splice(i, 1);
+      } else {
+        $scope.selectedUsers.push(user);
+      }
+    };
+
+    $scope.isSelected = (user) => $scope.selectedUsers.indexOf(user) >= 0;
+  }
 });
 
 registry.register(constant({
