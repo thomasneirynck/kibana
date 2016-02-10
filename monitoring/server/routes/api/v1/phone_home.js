5ffa04f41a136de9e1ba6f8b0ee33bc8c3081145
Timothy Sullivan <tsullivan@elastic.co>
2016-02-10 15:42:07 -0700

    marvel ui: deactivate local phone home indexing (dev mode)
    Original commit: elastic/x-pack@f69677ca56e06f44e4a8018b750319b37f45ff4c

--- monitoring/server/routes/api/v1/phone_home.js
+++ monitoring/server/routes/api/v1/phone_home.js
@@ -1,3 +1,6 @@
+/*
+ * This endpoint is ONLY for development and internal testing.
+ */
 const root = require('requirefrom')('');
 const handleError = root('server/lib/handle_error');
 
@@ -9,16 +12,21 @@ module.exports = (server) => {
     path: '/api/monitoring/v1/phone-home',
     method: 'POST',
     handler: (req, reply) => {
-      const body = req.payload;
-      const options = {
-        index: '.monitoring',
-        meta: 'route-phone_home',
-        type: 'phone_home',
-        body: body
-      };
-      callWithRequest(req, 'index', options)
-      .then(reply)
-      .catch(err => reply(handleError(err, req)));
+      // Change to true to test indexing the data. Note, user must have privileges
+      if (false) {
+        const body = req.payload;
+        const options = {
+          index: '.monitoring',
+          meta: 'route-phone_home',
+          type: 'phone_home',
+          body: body
+        };
+        callWithRequest(req, 'index', options)
+        .then(reply)
+        .catch(err => reply(handleError(err, req)));
+      } else {
+        reply({});
+      }
     }
   });
 };
