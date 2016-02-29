32c599fe185b77d28d6cd1fd85ac755a92efbeb6
Lukas Olson <olson.lukas@gmail.com>
2016-02-29 10:00:38 -0700

    Kibana/Shield: Initial edit role UI
    Original commit: elastic/x-pack@018d91ef58a158c3a79c043799e3bfcd87c38187

---
+++ shield/public/views/settings/edit_role.js
@@ -0,0 +1,60 @@
+import {clone} from 'lodash';
+import routes from 'ui/routes';
+import template from 'plugins/shield/views/settings/edit_role.html';
+import 'angular-resource';
+import 'plugins/shield/services/shield_user';
+import 'plugins/shield/services/shield_role';
+import 'plugins/shield/views/settings/edit_user.less';
+
+routes.when('/settings/security/roles/edit/:name?', {
+  template,
+  resolve: {
+    role($route, ShieldRole) {
+      const name = $route.current.params.name;
+      if (name != null) return ShieldRole.get({name});
+      return new ShieldRole({
+        cluster: [],
+        indices: []
+      });
+    },
+    users(ShieldUser) {
+      return ShieldUser.query();
+    }
+  },
+  controller($scope, $route, $location) {
+    $scope.isNewRole = $route.current.params.name == null;
+    $scope.role = $route.current.locals.role;
+    $scope.users = $route.current.locals.users;
+    $scope.newIndex = {name: '', privileges: []};
+    $scope.newPrivilege = '';
+
+    $scope.deleteRole = (role) => {
+      if (!confirm('Are you sure you want to delete this role? This action is irreversible!')) return;
+      role.$delete().then($scope.goToRoleList);
+    };
+
+    $scope.saveRole = (role) => {
+      role.$save().then($scope.goToRoleList);
+    };
+
+    $scope.goToRoleList = () => {
+      $location.path('/settings/security/roles');
+    };
+
+    $scope.removePrivilege = (index, privilege) => {
+      index.privileges.splice(index.privileges.indexOf(privilege), 1);
+    };
+
+    $scope.addPrivilege = (index, privilege) => {
+      index.privileges.push(privilege);
+    };
+
+    $scope.removeIndex = (role, index) => {
+      role.indices.splice(role.indices.indexOf(index), 1);
+    };
+
+    $scope.addIndex = (role, index) => {
+      role.indices.push(clone(index));
+    };
+  }
+});
--- shield/public/views/settings/settings.js
+++ shield/public/views/settings/settings.js
@@ -3,6 +3,7 @@ import registry from 'ui/registry/settings_sections';
 import 'plugins/shield/views/settings/users';
 import 'plugins/shield/views/settings/roles';
 import 'plugins/shield/views/settings/edit_user';
+import 'plugins/shield/views/settings/edit_role';
 
 registry.register(constant({
   order: 10,
