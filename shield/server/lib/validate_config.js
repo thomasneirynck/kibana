1b32dff7439aa9f9d67a2ecbeb6df0ed24174d54
Joe Fleming <joe.fleming@gmail.com>
2016-03-17 11:21:58 -0700

    fix goof in shield config params
    Original commit: elastic/x-pack@76d23ffdf3817ba18d4538d7b956e2cc1d0461f8

--- shield/server/lib/validate_config.js
+++ shield/server/lib/validate_config.js
@@ -1,10 +1,10 @@
 export default (config) => {
-  if (config.get('shield.encryptionKey') == null) {
+  if (config.get('xpack.shield.encryptionKey') == null) {
     throw new Error('shield.encryptionKey is required in kibana.yml.');
   }
 
-  const isSslConfigured = config.get('xpack.server.ssl.key') != null && config.get('xpack.server.ssl.cert') != null;
-  if (!isSslConfigured && !config.get('shield.skipSslCheck')) {
+  const isSslConfigured = config.get('server.ssl.key') != null && config.get('server.ssl.cert') != null;
+  if (!isSslConfigured && !config.get('xpack.shield.skipSslCheck')) {
     throw new Error('HTTPS is required. Please set server.ssl.key and server.ssl.cert in kibana.yml.');
   }
 };
