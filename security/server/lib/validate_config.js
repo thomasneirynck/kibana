5adf5947d004530c67123c7a777b0bcbbc32d637
Joe Fleming <joe.fleming@gmail.com>
2016-03-25 14:10:00 -0700

    tweak ssl message, update tests
    add tests for warning user about insecure sessions
    
    Original commit: elastic/x-pack@b1eda910966c7fd0e60c1f21842fda7ed77e933c

--- security/server/lib/validate_config.js
+++ security/server/lib/validate_config.js
@@ -6,7 +6,7 @@ export default (config, log) => {
   const isSslConfigured = config.get('server.ssl.key') != null && config.get('server.ssl.cert') != null;
   if (config.get('xpack.security.skipSslCheck')) {
     log('Skipping Kibana server SSL check');
-    if (!config.get('xpack.security.useUnsafeSession')) log('SSL is still required for this plugin to function');
+    if (!config.get('xpack.security.useUnsafeSession')) log('Note that SSL is required for this plugin to function');
   } else if (!isSslConfigured) {
     throw new Error('HTTPS is required. Please set server.ssl.key and server.ssl.cert in kibana.yml.');
   }
