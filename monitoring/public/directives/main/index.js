7a955f1ca27af7f67191d02f685e810bec00bbb2
Timothy Sullivan <tsullivan@elastic.co>
2016-03-31 14:12:06 -0700

    monitoring ui: main directive has name in scope
    Original commit: elastic/x-pack@bb1d113c266238350287046b6214a191952c4a7d

--- monitoring/public/directives/main/index.js
+++ monitoring/public/directives/main/index.js
@@ -1,16 +1,19 @@
 const app = require('ui/modules').get('plugins/monitoring/directives', []);
-app.directive('monitoringMain', (kbnUrl, $location) => {
+app.directive('monitoringMain', (kbnUrl) => {
   return {
     restrict: 'E',
     transclude: true,
     template: require('plugins/monitoring/directives/main/index.html'),
-    link: function (scope) {
+    link: function (scope, _el, attrs) {
+      scope.name = attrs.name;
       scope.isActive = function (testPath) {
-        return $location.path() === '/' + testPath;
+        return scope.name === testPath;
       };
 
       scope.routeTo = function (dest) {
-        kbnUrl.changePath(`/${dest}`);
+        if (dest !== scope.name) {
+          kbnUrl.changePath(`/${dest}`);
+        }
       };
     }
   };
