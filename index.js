d02cffdadd8ebc6cb27b69ec1469590372cc4ede
Timothy Sullivan <tsullivan@elastic.co>
2016-04-26 15:33:18 -0700

    ui: move “main” plugin function to plugins dir
    per feedback
    
    Original commit: elastic/x-pack@36672bdffdac8fd273cae93142d58641b7178b5b

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-import { join } from 'path';
-import requireAllAndApply from './server/lib/require_all_and_apply';
-import xpackInfo from './server/lib/xpack_info';
+import main from './plugins/main';
 import graph from './plugins/graph';
 import monitoring from './plugins/monitoring';
 import reporting from './plugins/reporting';
 import security from './plugins/security';
-
-function main(kibana) {
-  return new kibana.Plugin({
-    id: 'xpackMain',
-    require: ['elasticsearch'],
-    init: function (server) {
-      const client = server.plugins.elasticsearch.client; // NOTE: is an authenticated client
-      xpackInfo(client)
-      .then((info) => {
-        server.expose('info', info);
-        requireAllAndApply(join(__dirname, 'server', 'routes', '**', '*.js'), server);
-      });
-    }
-  });
-}
 
 module.exports = function (kibana) {
   return [
