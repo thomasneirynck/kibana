ee7139932432320987dd40d9cfe542dfa6aa6807
Timothy Sullivan <tsullivan@elastic.co>
2016-04-27 16:08:24 -0700

    xpack info: add “expiresSoon” boolean field
    Original commit: elastic/x-pack@824e444813f73ec79c0347ea0f6972dff1fedcd9

--- server/lib/xpack_info.js
+++ server/lib/xpack_info.js
@@ -1,15 +1,22 @@
 /* Call the XPack Info API for feature flags and mode of license
  * Requires an authenticated client
+ * Doesn't give detailed info about license
  */
+import moment from 'moment';
+import { get } from 'lodash';
 export default function xpackInfo(client) {
   return client.transport.request({
     method: 'GET',
     path: '_xpack'
   })
   .then(response => {
+    const expiryDateMillis = get(response, 'license.expiry_date_in_millis');
+    const expirySoonDate = moment.utc(expiryDateMillis).subtract(30, 'days');
+    const expiresSoon = moment.utc().isAfter(expirySoonDate);
     return {
       features: response.features,
-      mode: response.license.mode
+      mode: response.license.mode,
+      expiresSoon
     };
   });
 };
@@ -32,5 +39,6 @@ export default function xpackInfo(client) {
       { description: 'Alerting, Notification and Automation for the Elastic Stack',
         available: true,
         enabled: true } },
-  mode: 'trial' }
+  mode: 'trial',
+  expiresSoon: true }
  */
