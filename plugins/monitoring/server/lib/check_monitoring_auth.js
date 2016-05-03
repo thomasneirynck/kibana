ae320d8cfb1e3d66388b4e8aa5e23cd112c00f4b
Timothy Sullivan <tsullivan@elastic.co>
2016-05-03 14:22:48 -0700

    monitoring ui: app user permission check checks for “read” privilege
    Original commit: elastic/x-pack@53e04755ffba864167b1beca1dbf42140fcc07ff

--- plugins/monitoring/server/lib/check_monitoring_auth.js
+++ plugins/monitoring/server/lib/check_monitoring_auth.js
@@ -1,11 +1,23 @@
-// Permissions check. If authentication doesn't have privilege to view
+import Promise from 'bluebird';
+
+// Check "read" privileges on monitoring-*. If authentication doesn't have privilege to view
 // monitoring data, then this causes the permissions error, instead of
 // leaving fieldStats to cause a 404 error.
 export default function checkMonitoringAuth(req) {
   const callWithRequest = req.server.plugins.monitoring.callWithRequest;
   const config = req.server.config();
-  return callWithRequest(req, 'indices.getMapping', {
-    index: config.get('xpack.monitoring.index'),
-    type: 'cluster_info'
-  });
+
+  function checkDataIndex() {
+    return callWithRequest(req, 'count', {
+      index: config.get('xpack.monitoring.index')
+    });
+  }
+  function checkStatsIndices() {
+    return callWithRequest(req, 'count', {
+      index: `${config.get('xpack.monitoring.index_prefix')}*`
+    });
+  }
+
+  // run through checks in a series, so the first failure can reject the promise
+  return Promise.mapSeries([checkDataIndex(), checkStatsIndices()], () => true);
 };
