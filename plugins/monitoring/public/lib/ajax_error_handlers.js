73b13ee68c686e30e14290c4fd286c5b554defab
Timothy Sullivan <tsullivan@elastic.co>
2016-05-05 13:33:13 -0700

    monitoring ui: better unauth error message
    Original commit: elastic/x-pack@5ef615bce130b444889ae688680a14d24a055e95

--- plugins/monitoring/public/lib/ajax_error_handlers.js
+++ plugins/monitoring/public/lib/ajax_error_handlers.js
@@ -4,7 +4,9 @@ module.exports = function ajaxErrorHandlersProvider(Notifier) {
     fatalError(err) {
       if (err.status === 403) {
         const shieldNotifier = new Notifier({ location: 'Security Plugin' });
-        return shieldNotifier.fatal('Sorry, you are not authorized to access Monitoring');
+        return shieldNotifier.fatal(
+          'Sorry, you are not authorized to access Monitoring. The `monitoring_user` or equivalent role is required.'
+        );
       }
       const genericNotifier = new Notifier({ location: 'Monitoring' });
       return genericNotifier.fatal(err);
