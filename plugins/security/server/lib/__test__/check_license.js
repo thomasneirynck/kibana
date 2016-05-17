911b082fe25231ffde280dde6db8552652052da6
Shaunak Kashyap <ycombinator@gmail.com>
2016-05-17 08:39:33 -0700

    Using sinon for better stubbing of feature check method
    Original commit: elastic/x-pack@82861e7afd9f337d31136b2f0747371e4e8d3c8d

--- plugins/security/server/lib/__test__/check_license.js
+++ plugins/security/server/lib/__test__/check_license.js
@@ -12,13 +12,9 @@ describe('check_license', function () {
   });
 
   it ('should set showSecurityFeatures to false if security is disabled in Elasticsearch', () => {
-    set(mockLicenseInfo, 'feature', () => {
-      return {
-        isEnabled: () => {
-          return false;
-        }
-      };
-    });
+    set(mockLicenseInfo, 'feature', sinon.stub().withArgs('security').returns({
+      isEnabled: () => { return false; }
+    }));
     set(mockLicenseInfo, 'license.isActive', () => { return 'irrelevant'; });
     set(mockLicenseInfo, 'license.isOneOf', () => { return 'irrelevant'; });
 
@@ -28,26 +24,18 @@ describe('check_license', function () {
   it ('should set showSecurityFeatures to false if license is basic', () => {
     set(mockLicenseInfo, 'license.isOneOf', sinon.stub().withArgs([ 'basic' ]).returns(true));
     set(mockLicenseInfo, 'license.isActive', () => { return 'irrelevant'; });
-    set(mockLicenseInfo, 'feature', () => {
-      return {
-        isEnabled: () => {
-          return 'irrelevant';
-        }
-      };
-    });
+    set(mockLicenseInfo, 'feature', sinon.stub().withArgs('security').returns({
+      isEnabled: () => { return 'irrelevant'; }
+    }));
 
     expect(checkLicense(mockLicenseInfo).showSecurityFeatures).to.be(false);
   });
 
   it ('should set allowLogin to false if license has expired even if security is enabled in Elasticsearch and license is not basic', () => {
     set(mockLicenseInfo, 'license.isActive', () => { return false; });
-    set(mockLicenseInfo, 'feature', () => {
-      return {
-        isEnabled: () => {
-          return true;
-        }
-      };
-    });
+    set(mockLicenseInfo, 'feature', sinon.stub().withArgs('security').returns({
+      isEnabled: () => { return true; }
+    }));
     set(mockLicenseInfo, 'license.isOneOf', sinon.stub().withArgs([ 'basic' ]).returns(false));
 
     expect(checkLicense(mockLicenseInfo).allowLogin).to.be(false);
