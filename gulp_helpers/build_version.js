bcc91afe9db7b92cade6f8187ba01dc08230a6af
Joe Fleming <joe.fleming@gmail.com>
2016-05-17 11:29:23 -0700

    allow custom version via flag
    with masic semver validation
    
    Original commit: elastic/x-pack@906643b587e80ef83575f56ff980486f5dab01a2

--- gulp_helpers/build_version.js
+++ gulp_helpers/build_version.js
@@ -2,15 +2,17 @@ var fs = require('fs');
 var path = require('path');
 var properties = require('properties');
 var yargs = require('yargs');
+var semver = require('semver');
 var pkg = require('../package.json');
 
 yargs
 .alias('r', 'release').describe('r', 'Create a release build, not a snapshot')
+.alias('v', 'version').describe('v', 'Explicitely set the version');
 var argv = yargs.argv;
 
 var propFile = path.resolve('..', '..', 'elasticsearch', 'buildSrc', 'version.properties');
 
-function getVersion() {
+function getFileVersion() {
   var snapshotText = (argv.release) ? '' : '-SNAPSHOT';
 
   try {
@@ -22,6 +24,21 @@ function getVersion() {
   } catch (e) {
     return pkg.version.replace(/\-snapshot/i, snapshotText);
   }
+}
+
+function getVersion() {
+  var fileVersion = getFileVersion();
+  // ensure valid version
+  if (!argv.version) return fileVersion;
+  if (!semver.valid(argv.version)) throw new Error('Version is invalid: ' + argv.version);
+
+  // ensure version roughly matches
+  var minVer = '^' + fileVersion.split('-')[0];
+  var compareVer = argv.version.split('-')[0];
+
+  if (!semver.satisfies(compareVer, minVer)) throw new Error('Version does not match ' + minVer);
+
+  return argv.version;
 }
 
 module.exports = getVersion;
