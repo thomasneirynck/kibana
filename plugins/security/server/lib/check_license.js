f97f44e1293ce6aa6764432f04f5aac494723cd0
Shaunak Kashyap <ycombinator@gmail.com>
2016-05-18 23:21:07 -0500

    Make code more defensive
    Original commit: elastic/x-pack@de9aaf6189cd278f8d5e9d5ccbd5be19f7b33d89

--- plugins/security/server/lib/check_license.js
+++ plugins/security/server/lib/check_license.js
@@ -1,5 +1,15 @@
 export default function checkLicense(xpackLicenseInfo) {
 
+  // If, for some reason, we cannot get license information
+  // from Elasticsearch, assume worst-case and lock user
+  // at login screen.
+  if (!xpackLicenseInfo) {
+    return {
+      showSecurityFeatures: true,
+      allowLogin: false
+    };
+  }
+
   const isLicenseActive = xpackLicenseInfo.license.isActive();
   const isLicenseBasic = xpackLicenseInfo.license.isOneOf(['basic']);
   const isEnabledInES = xpackLicenseInfo.feature('security').isEnabled();
