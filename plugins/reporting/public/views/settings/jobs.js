b9f649a6976d0518c22327ee38466731f7a9a7b0
Joe Fleming <joe.fleming@gmail.com>
2016-05-20 12:33:02 -0700

    change interface a bit, map job documents
    Original commit: elastic/x-pack@07a9b773d20b56e1ea79966dfb82b03b4bdc8294

--- plugins/reporting/public/views/settings/jobs.js
+++ plugins/reporting/public/views/settings/jobs.js
@@ -3,6 +3,21 @@ import template from 'plugins/reporting/views/settings/jobs.html';
 
 import 'plugins/reporting/services/job_queue';
 
+function mapJobs(jobs) {
+  return jobs.map((job) => {
+    return {
+      id: job._id,
+      type: job._source.jobtype,
+      created_by: job._source.created_by,
+      created_at: job._source.created_at,
+      started_at: job._source.started_at,
+      completed_at: job._source.completed_at,
+      status: job._source.status,
+      content_type: job._source.output.content_type,
+    };
+  });
+}
+
 routes.when('/settings/reporting/jobs', {
   template,
   resolve: {
@@ -11,7 +26,7 @@ routes.when('/settings/reporting/jobs', {
     }
   },
   controller($scope, $route) {
-    $scope.jobs = $route.current.locals.jobs;
+    $scope.jobs = mapJobs($route.current.locals.jobs);
 
     $scope.download = (jobId) => {
       console.log('download', jobId);
