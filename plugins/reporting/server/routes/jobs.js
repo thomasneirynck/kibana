822169f21198cd858d60b9eb79b0ad4ee5abc575
Joe Fleming <joe.fleming@gmail.com>
2016-05-20 12:33:13 -0700

    add download route
    Original commit: elastic/x-pack@cd2c4c4c6a7bb92583899ab7fa5a994b8330ee95

--- plugins/reporting/server/routes/jobs.js
+++ plugins/reporting/server/routes/jobs.js
@@ -1,5 +1,7 @@
 const getUser = require('../lib/get_user');
 const jobsQueryFactory = require('../lib/jobs_query');
+const boom = require('boom');
+
 
 module.exports = function (server) {
   const jobsQuery = jobsQueryFactory(server);
@@ -17,6 +19,25 @@ module.exports = function (server) {
       .then((user) => jobsQuery.list(user, page, size));
 
       reply(results);
+    }
+  });
+
+  server.route({
+    path: `${mainEntry}/download/{docId}`,
+    method: 'GET',
+    handler: (request, reply) => {
+      const { docId } = request.params;
+
+      getUser(server, request)
+      .then((user) => jobsQuery.get(user, docId, true))
+      .then((doc) => {
+        if (!doc) return reply(boom.notFound());
+
+        const content = new Buffer(doc._source.output.content, 'base64');
+        const response = reply(content);
+        if (doc._source.output.content_type) response.type(doc._source.output.content_type);
+      });
+
     }
   });
 };
