cd2b086c1812a3726afc6c146c0fe276c17267b1
Shaunak Kashyap <ycombinator@gmail.com>
2016-05-26 15:48:01 -0700

    Removing outdated comment describing structure of returned object
    Original commit: elastic/x-pack@df0c90cdaa1b2e77358452b17c1a3cbf094b82ad

--- server/lib/xpack_info.js
+++ server/lib/xpack_info.js
@@ -46,25 +46,3 @@ export default function xpackInfo(client) {
     };
   });
 };
-
-/*
-{ features:
-   { graph:
-      { description: 'Graph Data Exploration for the Elastic Stack',
-        available: true,
-        enabled: true },
-     monitoring:
-      { description: 'Monitoring for the Elastic Stack',
-        available: true,
-        enabled: true },
-     security:
-      { description: 'Security for the Elastic Stack',
-        available: true,
-        enabled: true },
-     watcher:
-      { description: 'Alerting, Notification and Automation for the Elastic Stack',
-        available: true,
-        enabled: true } },
-  mode: 'trial',
-  expiresSoon: true }
- */
