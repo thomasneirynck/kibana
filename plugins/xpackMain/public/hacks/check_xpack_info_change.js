3c08f3266e0eb7a4e63030eac411af01871b16e7
Shaunak Kashyap <ycombinator@gmail.com>
2016-06-08 17:52:28 -0700

    Intercept error responses on the client-side as well
    Original commit: elastic/x-pack@e37024bee446ec53e59675bf3512e3577a8326e7

--- plugins/xpackMain/public/hacks/check_xpack_info_change.js
+++ plugins/xpackMain/public/hacks/check_xpack_info_change.js
@@ -1,50 +1,54 @@
+import { identity } from 'lodash';
 import uiModules from 'ui/modules';
 import chrome from 'ui/chrome';
 
 const module = uiModules.get('xpackMain', []);
 
-module.factory('checkXPackInfoChange', ($window, $injector) => {
+module.factory('checkXPackInfoChange', ($q, $window, $injector) => {
   let _isInfoUpdateInProgress = false;
-  return {
-    response: (response) => {
 
-      // If another instance of this response interceptor is
-      // already updating the info and signature, continue on
-      // to avoid an infinite loop
-      if (_isInfoUpdateInProgress) {
-        return response;
-      }
-
-      // Get xpack info signature in response; if it's empty, continue on...
-      const signatureFromServer = response.headers('kbn-xpack-sig');
-      if (!signatureFromServer) {
-        return response;
-      }
-
-      // Get xpack info signature from local storage
-      const localSignature = $window.localStorage.getItem('xpackMain.infoSignature');
-
-      // If they are the same, nothing to do; continue on...
-      if (localSignature === signatureFromServer) {
-        return response;
-      }
-
-      // Signatures differ so xpack info has changed on Kibana
-      // server. Fetch it and update local info + signature.
-      _isInfoUpdateInProgress = true;
-      const $http = $injector.get('$http'); // To prevent circular dependency Angular error
-      return $http.get(chrome.addBasePath('/api/xpack/v1/info'))
-      .then((xpackInfoResponse) => {
-        $window.localStorage.setItem('xpackMain.info', JSON.stringify(xpackInfoResponse.data));
-        $window.localStorage.setItem('xpackMain.infoSignature', xpackInfoResponse.headers('kbn-xpack-sig'));
-        _isInfoUpdateInProgress = false;
-        return response;
-      })
-      .catch((e) => {
-        _isInfoUpdateInProgress = false;
-        throw e;
-      });
+  function interceptor(response, handleResponse) {
+    // If another instance of this response interceptor is
+    // already updating the info and signature, continue on
+    // to avoid an infinite loop
+    if (_isInfoUpdateInProgress) {
+      return handleResponse(response);
     }
+
+    // Get xpack info signature in response; if it's empty, continue on...
+    const signatureFromServer = response.headers('kbn-xpack-sig');
+    if (!signatureFromServer) {
+      return handleResponse(response);
+    }
+
+    // Get xpack info signature from local storage
+    const localSignature = $window.localStorage.getItem('xpackMain.infoSignature');
+
+    // If they are the same, nothing to do; continue on...
+    if (localSignature === signatureFromServer) {
+      return handleResponse(response);
+    }
+
+    // Signatures differ so xpack info has changed on Kibana
+    // server. Fetch it and update local info + signature.
+    _isInfoUpdateInProgress = true;
+    const $http = $injector.get('$http'); // To prevent circular dependency Angular error
+    return $http.get(chrome.addBasePath('/api/xpack/v1/info'))
+    .then((xpackInfoResponse) => {
+      $window.localStorage.setItem('xpackMain.info', JSON.stringify(xpackInfoResponse.data));
+      $window.localStorage.setItem('xpackMain.infoSignature', xpackInfoResponse.headers('kbn-xpack-sig'));
+      _isInfoUpdateInProgress = false;
+      return handleResponse(response);
+    })
+    .catch((e) => {
+      _isInfoUpdateInProgress = false;
+      throw e;
+    });
+  }
+
+  return {
+    response: (response) => interceptor(response, identity),
+    responseError: (response) => interceptor(response, $q.reject)
   };
 });
 
