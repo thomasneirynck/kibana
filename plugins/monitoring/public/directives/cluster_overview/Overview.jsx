044a60cbdb2da369f92fec8a3c9d396497a31095
Timothy Sullivan <tsullivan@elastic.co>
2016-06-09 17:01:43 -0700

    monitoring ui: add link to license info to cluster overview
    Original commit: elastic/x-pack@d827a2c880490bebfe2a9512c6ea44f22e6782a9

--- plugins/monitoring/public/directives/cluster_overview/Overview.jsx
+++ plugins/monitoring/public/directives/cluster_overview/Overview.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import moment from 'moment-timezone';
 import statusIconClass from '../../lib/status_icon_class';
 const formatNumber = require('../../lib/format_number');
 
@@ -91,13 +92,18 @@ module.exports = React.createClass({
   getInitialState() {
     const scope = this.props.scope;
     const kbnChangePath = this.props.kbnUrl.changePath;
+    const angularChangeUrl = target => {
+      scope.$evalAsync(() => {
+        kbnChangePath(target);
+      });
+    };
     return {
       elasticsearch: { ...scope.cluster.elasticsearch },
       kibana: scope.cluster.kibana,
-      angularChangeUrl(target) {
-        scope.$evalAsync(() => {
-          kbnChangePath(target);
-        });
+      license: scope.cluster.license,
+      angularChangeUrl,
+      goToLicense() {
+        angularChangeUrl('/license');
       }
     };
   },
@@ -105,10 +111,16 @@ module.exports = React.createClass({
     this.props.scope.$watch('cluster', (cluster) => {
       this.setState({
         elasticsearch: { ...cluster.elasticsearch},
-        kibana: cluster.kibana});
+        kibana: cluster.kibana,
+        license: cluster.license
+      });
     });
   },
   render() {
+    const formatDateLocal = (input) => {
+      return moment.tz(input, moment.tz.guess()).format('LL');
+    };
+
     return (
       <div className='monitoring-view'>
         <div className='col-md-6'>
@@ -116,6 +128,13 @@ module.exports = React.createClass({
         </div>
         <div className='col-md-6'>
           <KibanaRow {...this.state.kibana} angularChangeUrl={this.state.angularChangeUrl}/>
+        </div>
+        <div className='col-md-12'>
+          <p>
+            Your { _.capitalize(this.state.license.type)
+            } license will expire on <a onClick={ this.state.goToLicense }> {
+            formatDateLocal(this.state.license.expiry_date) }.</a>
+          </p>
         </div>
       </div>
     );
