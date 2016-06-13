c0e3b05c9becd0c8ab535df6b3bb22830b2c161e
Joe Fleming <joe.fleming@gmail.com>
2016-06-13 11:25:10 -0700

    remove notification from document create service
    Original commit: elastic/x-pack@43767c28cf0196cb1d00c6ff12502885d55b3c24

--- plugins/reporting/public/services/document_create.js
+++ plugins/reporting/public/services/document_create.js
@@ -1,17 +1,11 @@
-const Notifier = require('ui/notify/notifier');
-
 const module = require('ui/modules').get('reporting/job_queue');
 module.service('reportingDocumentCreate', ($http, Promise, Private) => {
   const appInfo = Private(require('plugins/reporting/app_info'));
 
   return () => {
-    const genericNotifier = new Notifier({ location: 'Reporting' });
     const info = appInfo();
     if (!info.exportable) return Promise.reject(new Error('not exportable'));
 
-    return $http.get(info.reportUrl)
-    .then(() => {
-      genericNotifier.info('Document generation has been queued. You can track its progress under Settings.');
-    });
+    return $http.get(info.reportUrl);
   };
 });
