8848806815851100a04c3db72474d13e24a55d45
Shaunak Kashyap <ycombinator@gmail.com>
2016-06-20 14:55:34 -0700

    Not using $scope but using this instead so scope variables are namespaced by controller name
    Original commit: elastic/x-pack@e81e108c3067c118f1142b43f689d06f44a4382e

--- plugins/security/public/views/login/login.js
+++ plugins/security/public/views/login/login.js
@@ -8,29 +8,29 @@ import template from 'plugins/security/views/login/login.html';
 chrome
 .setVisible(false)
 .setRootTemplate(template)
-.setRootController('login', ($http, $scope, $window, secureCookies) => {
+.setRootController('login', function ($http, $window, secureCookies) {
   const next = parseNext($window.location);
   const isSecure = !!$window.location.protocol.match(/^https/);
+  const self = this;
 
   function setupScope() {
     const xpackInfo = JSON.parse($window.localStorage.getItem('xpackMain.info')) || {};
     const defaultLoginMessage = 'Login is currently disabled because the license could not be determined.';
-    $scope.allowLogin = get(xpackInfo, 'features.security.allowLogin', false);
-    $scope.loginMessage = get(xpackInfo, 'features.security.loginMessage', defaultLoginMessage);
-
-    $scope.isDisabled = !isSecure && secureCookies;
-    $scope.submit = (username, password) => {
-      $scope.error = false;
+    self.allowLogin = get(xpackInfo, 'features.security.allowLogin', false);
+    self.loginMessage = get(xpackInfo, 'features.security.loginMessage', defaultLoginMessage);
+    self.isDisabled = !isSecure && secureCookies;
+    self.submit = (username, password) => {
+      self.error = false;
       $http.post('./api/security/v1/login', {username, password}).then(
         () => $window.location.href = `.${next}`,
-        () => $scope.error = true
+        () => self.error = true
       );
     };
   }
 
   // Make an API call so xpack info is populated in local storage
   // by interceptor. This can be any call that doesn't require auth.
-  return $http.get(chrome.addBasePath('/api/xpack/v1/info'))
+  $http.get(chrome.addBasePath('/api/xpack/v1/info'))
   .then(setupScope)
   .catch(setupScope);
 
