3f63822bce1cd89b0cffb5f891668af8bd393810
Shaunak Kashyap <ycombinator@gmail.com>
2016-06-20 07:02:25 -0700

    Move function definition to top of scope rather than rely on hoisting
    Original commit: elastic/x-pack@31b905627494d8eadea0732394abbb263c7befac

--- server/lib/mirror_plugin_status.js
+++ server/lib/mirror_plugin_status.js
@@ -1,13 +1,14 @@
 export default function mirrorPluginStatus(upstreamPlugin, downstreamPlugin, ...statesToMirror) {
-  if (statesToMirror.length === 0) {
-    statesToMirror.push('change');
-  }
-
-  statesToMirror.map(state => upstreamPlugin.status.on(state, mirror));
 
   function mirror() {
     const { state, message } = upstreamPlugin.status;
     downstreamPlugin.status[state](message);
   }
+
+  if (statesToMirror.length === 0) {
+    statesToMirror.push('change');
+  }
+
+  statesToMirror.map(state => upstreamPlugin.status.on(state, mirror));
   mirror(); // initial mirroring
 }
