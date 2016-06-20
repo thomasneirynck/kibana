bf1412cd2b6acdba2d1636c21cd207577b75660c
Shaunak Kashyap <ycombinator@gmail.com>
2016-06-20 14:26:55 -0700

    Only call state function if state is defined
    Original commit: elastic/x-pack@22a971db0a6f3098c26ae58619d3fd6da38b7527

--- server/lib/mirror_plugin_status.js
+++ server/lib/mirror_plugin_status.js
@@ -1,7 +1,9 @@
 export default function mirrorPluginStatus(upstreamPlugin, downstreamPlugin, ...statesToMirror) {
 
   function mirror(previousState, previousMsg, newState, newMsg) {
-    downstreamPlugin.status[newState](newMsg);
+    if (newState) {
+      downstreamPlugin.status[newState](newMsg);
+    }
   }
 
   if (statesToMirror.length === 0) {
