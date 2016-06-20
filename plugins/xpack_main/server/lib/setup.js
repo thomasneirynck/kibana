39b99445974912bf0f1ac2437926d0b4e9437697
Shaunak Kashyap <ycombinator@gmail.com>
2016-06-20 07:26:16 -0700

    Use _.once to implement singleton for registering onPreResponse handler
    Original commit: elastic/x-pack@e72971d0363502a29d450979e228f78f71c9e888

--- plugins/xpack_main/server/lib/setup.js
+++ plugins/xpack_main/server/lib/setup.js
@@ -4,8 +4,10 @@ import xpackInfo from '../../../../server/lib/xpack_info';
 import xpackUsage from '../../../../server/lib/xpack_usage';
 import injectXPackInfoSignature from './inject_xpack_info_signature';
 
-let preResponseHandlerRegistered = false;
 let xpackInfoSingleton = once(xpackInfo);
+let registerPreResponseHandlerSingleton = once((server, info) => {
+  server.ext('onPreResponse', partial(injectXPackInfoSignature, info));
+});
 
 export default function setup(server, xpackMainPlugin) {
   const client = server.plugins.elasticsearch.client; // NOTE: authenticated client using server config auth
@@ -19,11 +21,7 @@ export default function setup(server, xpackMainPlugin) {
     return info;
   })
   .then(info => {
-    // Register the onPreResponse handler, but only once
-    if (!preResponseHandlerRegistered) {
-      server.ext('onPreResponse', partial(injectXPackInfoSignature, info));
-      preResponseHandlerRegistered = true;
-    }
+    registerPreResponseHandlerSingleton(server, info);
   })
   .then(() => xpackMainPlugin.status.green('Ready'))
   .catch(reason => {
