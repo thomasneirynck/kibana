9ef04ce446344e480ebf0edab30a64469bda4dd3
Timothy Sullivan <tsullivan@elastic.co>
2016-06-22 12:54:13 -0700

    monitoring ui: check if current license is basic and multi-cluster monitoring should be restricted
    Original commit: elastic/x-pack@d7c4e6754a29ef719f69589836cf4e18ce3d2bc1

--- plugins/monitoring/public/lib/route_init.js
+++ plugins/monitoring/public/lib/route_init.js
@@ -2,6 +2,23 @@ var _ = require('lodash');
 module.exports = function routeInitProvider(Notifier, Private, monitoringClusters, globalState, license, kbnUrl) {
   var phoneHome = Private(require('plugins/monitoring/lib/phone_home'));
   var ajaxErrorHandlers = Private(require('plugins/monitoring/lib/ajax_error_handlers'));
+
+  // check if the license is expired and if anything needs to be done about it
+  function isLicenseFresh(expiryDateInMillis) {
+    const isExpired = (new Date()).getTime() > expiryDateInMillis;
+    const alreadyOnLicensePage = _.contains(window.location.hash, 'license');
+    return !isExpired || alreadyOnLicensePage;
+  }
+
+  // check if the setup is multi-cluster mode, and that mode is supported
+  function isMultiClusterSupported(isBasic, clusters) {
+    const onClusterListing = _.contains(window.location.hash, 'home');
+    if (clusters.length > 1 && isBasic && !onClusterListing) {
+      return false;
+    }
+    return true;
+  }
+
   return function () {
     var notify = new Notifier({ location: 'Monitoring' });
     return monitoringClusters()
@@ -22,22 +39,27 @@ module.exports = function routeInitProvider(Notifier, Private, monitoringCluster
         return null;
       }());
 
-      if (cluster) {
+      if (cluster && cluster.license) {
         globalState.cluster_uuid = cluster.cluster_uuid;
         globalState.save();
       } else {
-        notify.error('We can\'t seem to find any clusters in your Monitoring data. Please check your Monitoring agents');
+        notify.error('We can\'t seem to find any valid clusters in your Monitoring data. Please check your Monitoring agents');
         return kbnUrl.redirect('/no-data');
       }
 
       const clusterLicense = cluster.license;
-      const isExpired = (new Date()).getTime() > clusterLicense.expiry_date_in_millis;
-
-      if (isExpired && !_.contains(window.location.hash, 'license')) {
-        // redirect to license, but avoid infinite loop
-        kbnUrl.redirect('/license');
-      }
       license.setLicenseType(clusterLicense.type);
+
+      // check license expiration
+      if (!isLicenseFresh(clusterLicense.expiry_date_in_millis)) {
+        return kbnUrl.redirect('/license');
+      }
+
+      // check if multi-cluster monitoring, and if its allowed
+      if (!isMultiClusterSupported(license.isBasic(), clusters)) {
+        return kbnUrl.redirect('/home');
+      }
+
       return clusters;
     })
     .catch(ajaxErrorHandlers.fatalError);
