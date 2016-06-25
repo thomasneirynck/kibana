4faf8a640c0ab282dcda752f4b496e8fb8fcc21d
Shaunak Kashyap <ycombinator@gmail.com>
2016-06-24 18:28:35 -0700

    Implement license checking UX for reporting
    Original commit: elastic/x-pack@0bd6903b13601318c955827b0099328094b5432b

--- plugins/reporting/public/controls/visualize.js
+++ plugins/reporting/public/controls/visualize.js
@@ -1,9 +1,10 @@
+import 'plugins/xpack_main/services/xpack_info';
 require('plugins/reporting/services/document_create');
 const Notifier = require('ui/notify/notifier');
 const navbarExtensions = require('ui/registry/navbar_extensions');
 navbarExtensions.register(visualizeReportProvider);
 
-function visualizeReportProvider(reportingDocumentCreate) {
+function visualizeReportProvider(reportingDocumentCreate, xpackInfo) {
   const genericNotifier = new Notifier({ location: 'Reporting' });
 
   return {
@@ -12,6 +13,9 @@ function visualizeReportProvider(reportingDocumentCreate) {
     order: 0,
     template: require('plugins/reporting/controls/export_button.html'),
     link: function visualizeReport($scope) {
+      $scope.showLinks = xpackInfo.get('features.reporting.showLinks', false);
+      $scope.enableLinks = xpackInfo.get('features.reporting.enableLinks', false);
+      $scope.disabledMessage = xpackInfo.get('features.reporting.message');
       $scope.handleClick = function () {
         return reportingDocumentCreate()
         .catch((err) => {
