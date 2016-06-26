f5e91f984bfdf59666f34453f5562e6d7df32689
Shaunak Kashyap <ycombinator@gmail.com>
2016-06-26 07:46:00 -0700

    Notify and redirect user if license does not support reporting
    Original commit: elastic/x-pack@a0d510abb3c2144b911d8694d24a0a972d6a8cef

--- plugins/reporting/public/services/job_queue.js
+++ plugins/reporting/public/services/job_queue.js
@@ -1,12 +1,27 @@
 import Notifier from 'ui/notify/notifier';
+import 'plugins/xpack_main/services/xpack_info';
 
 const module = require('ui/modules').get('reporting/job_queue');
 
-module.service('reportingJobQueue', ($http) => {
+module.service('reportingJobQueue', ($http, xpackInfo, kbnUrl) => {
   const baseUrl = '../api/reporting/jobs';
   const genericNotifier = new Notifier({ location: 'Reporting' });
 
+  function licenseAllowsToShowThisPage() {
+    return xpackInfo.get('features.reporting.showLinks')
+      && xpackInfo.get('features.reporting.enableLinks');
+  }
+
+  function notifyAndRedirectToManagementOverviewPage() {
+    genericNotifier.error(xpackInfo.get('features.reporting.message'));
+    kbnUrl.redirect('/management');
+  }
+
   function showError(err) {
+    if (!licenseAllowsToShowThisPage()) {
+      notifyAndRedirectToManagementOverviewPage();
+      return Promise.reject();
+    }
     var msg = err.statusText || 'Request failed';
     genericNotifier.error(msg);
     throw err;
