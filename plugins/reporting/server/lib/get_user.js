610cb397c24f2710276f832175df421b41eae50c
Joe Fleming <joe.fleming@gmail.com>
2016-06-30 10:38:35 -0700

    make get_user a proper factory
    use once_per_server
    
    Original commit: elastic/x-pack@227c6714e3c71b3688e396882e29de69b0b8c5f8

--- plugins/reporting/server/lib/get_user.js
+++ plugins/reporting/server/lib/get_user.js
@@ -1,6 +1,9 @@
 const { get, noop } = require('lodash');
+const oncePerServer = require('./once_per_server');
 
-module.exports = (server, request) => {
-  const getUser = get(server.plugins, 'security.getUser', noop);
-  return Promise.resolve(getUser(request));
-};
+module.exports = oncePerServer((server) => {
+  return (request) => {
+    const getUser = get(server.plugins, 'security.getUser', noop);
+    return Promise.resolve(getUser(request));
+  };
+});
--- plugins/reporting/server/routes/jobs.js
+++ plugins/reporting/server/routes/jobs.js
@@ -1,9 +1,11 @@
-const getUser = require('../lib/get_user');
+const getUserFactory = require('../lib/get_user');
 const jobsQueryFactory = require('../lib/jobs_query');
 const boom = require('boom');
 
 module.exports = function (server, commonRouteConfig) {
   const jobsQuery = jobsQueryFactory(server);
+  const getUser = getUserFactory(server);
+
   const mainEntry = '/api/reporting/jobs';
 
   server.route({
@@ -13,7 +15,7 @@ module.exports = function (server, commonRouteConfig) {
       const page = parseInt(request.query.page) || 0;
       const size = Math.min(100, parseInt(request.query.size) || 10);
 
-      const results = getUser(server, request)
+      const results = getUser(request)
       .then((user) => jobsQuery.list(user, page, size));
 
       reply(results);
@@ -27,7 +29,7 @@ module.exports = function (server, commonRouteConfig) {
     path: `${mainEntry}/count`,
     method: 'GET',
     handler: (request, reply) => {
-      const results = getUser(server, request)
+      const results = getUser(request)
       .then((user) => jobsQuery.count(user));
 
       reply(results);
@@ -43,7 +45,7 @@ module.exports = function (server, commonRouteConfig) {
     handler: (request, reply) => {
       const { docId } = request.params;
 
-      getUser(server, request)
+      getUser(request)
       .then((user) => jobsQuery.get(user, docId, true))
       .then((doc) => {
         if (!doc) return reply(boom.notFound());
@@ -61,7 +63,7 @@ module.exports = function (server, commonRouteConfig) {
     handler: (request, reply) => {
       const { docId } = request.params;
 
-      getUser(server, request)
+      getUser(request)
       .then((user) => jobsQuery.get(user, docId, true))
       .then((doc) => {
         if (!doc || doc._source.status !== 'completed') return reply(boom.notFound());
