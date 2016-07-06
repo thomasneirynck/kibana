c185f4ee04766fb0a735f3d8495b8ecb4249d524
Chris Earle <chris.earle@elastic.co>
2016-07-06 01:06:19 -0400

    [Monitoring] Decouple Kibana Stats Publishing from Application Version
    This changes the way that Kibana (and future applications) send their monitoring stats to Elasticsearch.
    
    Instead of sending their payloads with the System ID (e.g., "kibana") and System Version (e.g., "5.0.0-alpha4"), it now expects the System ID and System _API_ Version (e.g., "2"). This means a few things:
    
    - Future releases are automatically compatible with previous releases as long as the API version doesn't change.
    - Users don't have to update Kibana at the exact same time as their cluster (which technically means rolling updates were temporarily blockers of Kibana monitoring before).
    - We can accept old API versions (if we need to make a breaking change) and automatically up-convert them to the latest API version. (We are in full control of how far back we choose to accept)
    
    In general, this change implies that users should be updating their Monitoring cluster before their _monitored_ cluster(s) to get the best opportunity of monitoring backwards compatibility. That way if any API change does occur, then it can up-convert as needed. Then, any ES node should be updated, and only then should Kibana be updated. This is not required in any way, but it will give the smoothest experience.
    
    Original commit: elastic/x-pack@78b31360fd49f93e57135c429b9149a1290e76ad

---
+++ plugins/monitoring/lib/constants.js
@@ -0,0 +1,15 @@
+/**
+ * The Monitoring API version is the expected API format that we export and expect to import.
+ * @type {string}
+ */
+export const MONITORING_SYSTEM_API_VERSION = '2';
+/**
+ * The name of the Kibana System ID used to publish Kibana stats through the Monitoring system.
+ * @type {string}
+ */
+export const KIBANA_SYSTEM_ID = 'kibana';
+/**
+ * The type name used within the Monitoring index to publish Kibana stats.
+ * @type {string}
+ */
+export const KIBANA_STATS_TYPE = 'kibana_stats';
