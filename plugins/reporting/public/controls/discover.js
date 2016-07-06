4abd6b8fd94250b618b170258d507a97720ba1ab
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-06 13:52:39 -0700

    Implement license checking UX for Reporting buttons in top nav
    Original commit: elastic/x-pack@609cd7b3c39f373b51000bbd89d7a27018461881

--- plugins/reporting/public/controls/discover.js
+++ plugins/reporting/public/controls/discover.js
@@ -1,8 +1,10 @@
 require('plugins/reporting/directives/export_config');
+import XPackInfoProvider from 'plugins/xpack_main/services/xpack_info';
 
 const navbarExtensions = require('ui/registry/navbar_extensions');
 
-function discoverReportProvider() {
+function discoverReportProvider(Private) {
+  const xpackInfo = Private(XPackInfoProvider);
   return {
     appName: 'discover',
 
@@ -10,6 +12,9 @@ function discoverReportProvider() {
     label: 'Reporting',
     template: '<export-config object-type="Search"></export-config>',
     description: 'Search Report',
+    hideButton: () => !xpackInfo.get('features.reporting.showLinks', false),
+    disableButton: () => !xpackInfo.get('features.reporting.enableLinks', false),
+    tooltip: () => xpackInfo.get('features.reporting.message')
   };
 }
 
