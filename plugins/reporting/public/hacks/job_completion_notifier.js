832333313a806e706cfe0e214eefc535751060ea
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-09 02:29:14 -0700

    Refactoring: extract constants into constants module
    Original commit: elastic/x-pack@04a1eac191c14c7e5da23dea2b5898cf542b5e8a

--- plugins/reporting/public/hacks/job_completion_notifier.js
+++ plugins/reporting/public/hacks/job_completion_notifier.js
@@ -1,32 +1,29 @@
 import notify from 'ui/notify';
 import uiModules from 'ui/modules';
 import { last } from 'lodash';
-
-const CHECK_FREQUENCY_IN_MS = 10000;
-const STORAGE_KEY_LAST_CHECK = 'xpack.reporting.jobCompletionLastCheckedOn';
-const API_BASE_URL = '../api/reporting/jobs';
+import constants from '../../server/lib/constants.js';
 
 uiModules.get('kibana')
 .run(($http) => {
   setInterval(function startChecking() {
     getJobsCompletedSinceLastCheck($http)
     .then(jobs => jobs.forEach(showCompletionNotification));
-  }, CHECK_FREQUENCY_IN_MS);
+  }, constants.JOB_COMPLETION_CHECK_FREQUENCY_IN_MS);
 });
 
 function getLastCheckedOn() {
-  return window.localStorage.getItem(STORAGE_KEY_LAST_CHECK);
+  return window.localStorage.getItem(constants.JOB_COMPLETION_STORAGE_KEY_LAST_CHECK);
 }
 
 function setLastCheckedOn(newValue) {
-  window.localStorage.setItem(STORAGE_KEY_LAST_CHECK, newValue);
+  window.localStorage.setItem(constants.JOB_COMPLETION_STORAGE_KEY_LAST_CHECK, newValue);
 }
 
 function getJobsCompletedSinceLastCheck($http) {
   const lastCheckedOn = getLastCheckedOn();
 
   // Get all jobs in "completed" status since last check, sorted by completion time
-  const url = `${API_BASE_URL}/list_completed_since?since=${lastCheckedOn}`;
+  const url = `${constants.API_BASE_URL}/list_completed_since?since=${lastCheckedOn}`;
   return $http.get(url)
   .then(res => {
     res = res.data;
