17272a5547eb4288889465088d6755d818f1d59c
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-20 07:54:48 -0700

    Register/deregister security management items depending on whether there's an auth'd user
    Original commit: elastic/x-pack@3d1ee81a7a1904d206fe523b46b0474c0a51951d

--- plugins/security/public/views/management/management.js
+++ plugins/security/public/views/management/management.js
@@ -3,19 +3,31 @@ import 'plugins/security/views/management/roles';
 import 'plugins/security/views/management/edit_user';
 import 'plugins/security/views/management/edit_role';
 import 'plugins/security/views/management/management.less';
+import routes from 'ui/routes';
 
 import management from 'ui/management';
 
-const elasticsearch = management.getSection('elasticsearch');
+routes.defaults(/\/management/, {
+  resolve: {
+    managementSection: function (ShieldUser) {
+      const elasticsearch = management.getSection('elasticsearch');
+      const showSecurityLinks = !!ShieldUser.getCurrent();
 
-elasticsearch.register('users', {
-  order: 10,
-  display: 'Users',
-  url: '#/management/elasticsearch/users'
-});
+      elasticsearch.deregister('users');
+      elasticsearch.deregister('roles');
+      if (showSecurityLinks) {
+        elasticsearch.register('users', {
+          order: 10,
+          display: 'Users',
+          url: '#/management/elasticsearch/users'
+        });
 
-elasticsearch.register('roles', {
-  order: 20,
-  display: 'Roles',
-  url: '#/management/elasticsearch/roles'
+        elasticsearch.register('roles', {
+          order: 20,
+          display: 'Roles',
+          url: '#/management/elasticsearch/roles'
+        });
+      }
+    }
+  }
 });
