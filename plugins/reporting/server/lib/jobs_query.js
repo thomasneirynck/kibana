ab4f0d153c4c533f3459149e5aadc46defa939ff
Joe Fleming <joe.fleming@gmail.com>
2016-07-22 16:24:15 -0700

    pass request into job queries
    use the request object to pass authorization headers to the request
    picked from elastic/x-pack@97a16002
    
    Original commit: elastic/x-pack@97259090ac5f02c6bebf2c219c3f60c00cfc30a0

--- plugins/reporting/server/lib/jobs_query.js
+++ plugins/reporting/server/lib/jobs_query.js
@@ -1,11 +1,13 @@
 const { get } = require('lodash');
-const { QUEUE_INDEX, QUEUE_DOCTYPE } = require('./constants');
 const oncePerServer = require('./once_per_server');
+const { QUEUE_INDEX, QUEUE_DOCTYPE } = require('./constants');
+const getUserFactory = require('../lib/get_user');
 const defaultSize = 10;
 
 function jobsQueryFactory(server) {
   const esErrors = server.plugins.elasticsearch.errors;
   const client = server.plugins.elasticsearch.client;
+  const getUser = getUserFactory(server);
   const nouser = false;
 
   function execQuery(type, body) {
@@ -39,113 +41,126 @@ function jobsQueryFactory(server) {
   }
 
   return {
-    list(user, page = 0, size = defaultSize) {
-      const username = get(user, 'username', nouser);
+    list(request, page = 0, size = defaultSize) {
+      getUser(request)
+      .then((user) => {
+        const username = get(user, 'username', nouser);
 
-      const body = {
-        query: {
-          constant_score: {
-            filter: {
-              bool: {
-                should: [
-                  { term: { created_by: username } },
-                  { term: { created_by: nouser } },
-                ]
+        const body = {
+          query: {
+            constant_score: {
+              filter: {
+                bool: {
+                  should: [
+                    { term: { created_by: username } },
+                    { term: { created_by: nouser } },
+                  ]
+                }
               }
             }
-          }
-        },
-        from: size * page,
-        size: size,
-      };
+          },
+          from: size * page,
+          size: size,
+        };
 
-      return getHits(execQuery('search', body));
-    },
-
-    listCompletedSince(user, size = defaultSize, sinceInMs) {
-      const username = get(user, 'username', nouser);
-
-      const body = {
-        query: {
-          constant_score: {
-            filter: {
-              bool: {
-                should: [
-                  { term: { created_by: username } },
-                  { term: { created_by: nouser } },
-                ],
-                must: [
-                  { range: { completed_at: { gt: sinceInMs, format: 'epoch_millis' } } }
-                ]
-              }
-            }
-          }
-        },
-        size: size,
-        sort: { completed_at: 'asc' }
-      };
-
-      return getHits(execQuery('search', body));
-    },
-
-    count(user) {
-      const username = get(user, 'username', nouser);
-
-      const body = {
-        query: {
-          constant_score: {
-            filter: {
-              bool: {
-                should: [
-                  { term: { created_by: username } },
-                  { term: { created_by: nouser } },
-                ]
-              }
-            }
-          }
-        }
-      };
-
-      return execQuery('count', body)
-      .then((doc) => {
-        if (!doc) return 0;
-        return doc.count;
+        return getHits(execQuery('search', body));
       });
     },
 
-    get(user, id, includeContent = false) {
-      if (!id) return;
-      const username = get(user, 'username', nouser);
+    listCompletedSince(request, size = defaultSize, sinceInMs) {
+      getUser(request)
+      .then((user) => {
+        const username = get(user, 'username', nouser);
 
-      const body = {
-        query: {
-          constant_score: {
-            filter: {
-              bool: {
-                should: [
-                  { term: { created_by: nouser } },
-                  { term: { created_by: username } },
-                ],
-                filter: [
-                  { term: { _id: id } },
-                ],
+        const body = {
+          query: {
+            constant_score: {
+              filter: {
+                bool: {
+                  should: [
+                    { term: { created_by: username } },
+                    { term: { created_by: nouser } },
+                  ],
+                  must: [
+                    { range: { completed_at: { gt: sinceInMs, format: 'epoch_millis' } } }
+                  ]
+                }
+              }
+            }
+          },
+          size: size,
+          sort: { completed_at: 'asc' }
+        };
+
+        return getHits(execQuery('search', body));
+      });
+    },
+
+    count(request) {
+      getUser(request)
+      .then((user) => {
+        const username = get(user, 'username', nouser);
+
+        const body = {
+          query: {
+            constant_score: {
+              filter: {
+                bool: {
+                  should: [
+                    { term: { created_by: username } },
+                    { term: { created_by: nouser } },
+                  ]
+                }
               }
             }
           }
-        },
-        size: 1,
-      };
-
-      if (includeContent) {
-        body._source = {
-          exclude: []
         };
-      }
 
-      return getHits(execQuery('search', body))
-      .then((hits) => {
-        if (hits.length !== 1) return;
-        return hits[0];
+        return execQuery('count', body)
+        .then((doc) => {
+          if (!doc) return 0;
+          return doc.count;
+        });
+      });
+    },
+
+    get(request, id, includeContent = false) {
+      if (!id) return Promise.resolve();
+
+      getUser(request)
+      .then((user) => {
+        const username = get(user, 'username', nouser);
+
+        const body = {
+          query: {
+            constant_score: {
+              filter: {
+                bool: {
+                  should: [
+                    { term: { created_by: nouser } },
+                    { term: { created_by: username } },
+                  ],
+                  filter: [
+                    { term: { _id: id } },
+                  ],
+                }
+              }
+            }
+          },
+          size: 1,
+        };
+
+        if (includeContent) {
+          body._source = {
+            exclude: []
+          };
+        }
+
+        return getHits(execQuery('search', body))
+        .then((hits) => {
+          if (hits.length !== 1) return;
+          return hits[0];
+        });
       });
     }
   };
