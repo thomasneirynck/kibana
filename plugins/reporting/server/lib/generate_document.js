c8299d0cb4846cd549bae3c75f5c3ae7cefe257d
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-22 21:52:51 -0700

    Add time range to title
    Original commit: elastic/x-pack@147bddb62fa1ce3afafc2415a9f690eed6b107c7

--- plugins/reporting/server/lib/generate_document.js
+++ plugins/reporting/server/lib/generate_document.js
@@ -1,5 +1,6 @@
 const Promise = require('bluebird');
 const _ = require('lodash');
+import getTimeFilterRange from './get_time_filter_range';
 
 const pdf = require('./pdf');
 const oncePerServer = require('./once_per_server');
@@ -14,6 +15,10 @@ function generateDocumentFactory(server) {
 
   function printablePdf(title, savedObjects, query, headers) {
     const pdfOutput = pdf.create();
+
+    const timeRange = getTimeFilterRange(savedObjects, query);
+    title += (timeRange) ? ` — ${timeRange.from} to ${timeRange.to}` : '';
+    pdfOutput.setTitle(title);
 
     return Promise.map(savedObjects, function (savedObj) {
       if (savedObj.isMissing) {
