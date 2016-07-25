a0a030e0286aafb61c66b4f832ed65e9136f9f52
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-25 09:15:30 -0700

    If security is disabled, do not attempt to call the authenticate ES API
    Original commit: elastic/x-pack@e4fc96a320778d307b953b22d752c47d7fceec67

--- plugins/security/server/lib/create_expose.js
+++ plugins/security/server/lib/create_expose.js
@@ -3,5 +3,11 @@ import getClient from './get_client_shield';
 export default function createExports(server) {
   const callWithRequest = getClient(server).callWithRequest;
 
-  server.expose('getUser', (request) => callWithRequest(request, 'shield.authenticate'));
+  server.expose('getUser', (request) => {
+    const xpackInfo = server.plugins.xpack_main.info;
+    if (xpackInfo && xpackInfo.isAvailable() && !xpackInfo.feature('security').isEnabled()) {
+      return null;
+    }
+    return callWithRequest(request, 'shield.authenticate');
+  });
 };
