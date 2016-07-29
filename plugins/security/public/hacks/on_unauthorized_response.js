0eb5cb1197ca22afbedbd45f38dc4d990c4856b6
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-29 16:36:16 -0700

    Generalize notion of unauthorized response being allowed from some APIs
    Original commit: elastic/x-pack@735d90f904a1b4cf7eccec4f8bae0ab2c7e53fff

--- plugins/security/public/hacks/on_unauthorized_response.js
+++ plugins/security/public/hacks/on_unauthorized_response.js
@@ -1,14 +1,20 @@
 import { identity } from 'lodash';
 import uiModules from 'ui/modules';
 
-function isResponseFromLoginApi(response) {
-  return response.config.url.includes('/api/security/v1/login');
+function isUnauthorizedResponseAllowed(response) {
+  const APIS_ALLOWED_TO_RETURN_UNAUTHORIZED_RESPONSE = [
+    '/api/security/v1/login'
+  ];
+
+  return APIS_ALLOWED_TO_RETURN_UNAUTHORIZED_RESPONSE.reduce((isAllowed, endpoint) => {
+    return isAllowed || response.config.url.includes(endpoint);
+  }, false);
 }
 
 const module = uiModules.get('security');
 module.factory('onUnauthorizedResponse', ($q, $window, chrome) => {
   function interceptor(response, handleResponse) {
-    if (response.status === 401 && !isResponseFromLoginApi(response)) {
+    if (response.status === 401 && !isUnauthorizedResponseAllowed(response)) {
       const next = chrome.removeBasePath(`${window.location.pathname}${window.location.hash}`);
       const msg = 'SESSION_EXPIRED';
       $window.location.href = chrome.addBasePath(`/logout?next=${encodeURIComponent(next)}&msg=${encodeURIComponent(msg)}`);
