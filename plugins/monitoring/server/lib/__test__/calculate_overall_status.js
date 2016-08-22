334f42d635b8726a5d47edd82138c343be59cd7e
Timothy Sullivan <tsullivan@elastic.co>
2016-08-22 14:51:26 -0700

    monitoring ui: remove “health” check for cluster listing
    Allows viewing of stale monitoring data
    Allows listing of clusters that have had license deleted
    Replaces https://github.com/elastic/x-plugins/pull/2811
    
    Original commit: elastic/x-pack@684c05a97efa7b73dd46d2104892586a916fcc89

--- plugins/monitoring/server/lib/__test__/calculate_overall_status.js
+++ plugins/monitoring/server/lib/__test__/calculate_overall_status.js
@@ -4,15 +4,18 @@ import calculateKibanaStatus from '../calculate_overall_status';
 describe('Calculate Kibana Cluster Helath', () => {
   it('health status combined from multiple instances', () => {
     const greens = [
+      ['green', 'green', null],
       ['green', 'green'],
       ['green']
     ];
     const yellows = [
+      ['yellow', 'green', null],
       ['yellow', 'green'],
       ['green', 'yellow'],
       ['yellow']
     ];
     const reds = [
+      ['green', 'red', 'green', null],
       ['green', 'red', 'green'],
       ['red', 'yellow', 'green'],
       ['yellow', 'red', 'yellow'],
--- plugins/monitoring/server/lib/calculate_overall_status.js
+++ plugins/monitoring/server/lib/calculate_overall_status.js
@@ -4,6 +4,7 @@
  */
 export default function calculateOverallStatus(set) {
   return set.reduce((result, current) => {
+    if (!current) return result;
     if (current === 'red') return current; // change to red
     if (result !== 'green') return result; // preserve non-green
     return current; // change to green or yellow
