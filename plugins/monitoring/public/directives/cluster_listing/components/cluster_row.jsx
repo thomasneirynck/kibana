cba60ef1ceb2de0038f6a3a45eb2da2e5bdf4106
Timothy Sullivan <tsullivan@elastic.co>
2016-08-22 15:03:30 -0700

    monitoring ui:cluster listing treatment for clusters w/ invalid and unsupported license
    Breaks up the cluster row JSX, to make it more modular and have it
    better able to support multiple branches of conditional logic
    
    Original commit: elastic/x-pack@578a44b961ba0deadce89ec680c54dea2b889b1f

--- plugins/monitoring/public/directives/cluster_listing/components/cluster_row.jsx
+++ plugins/monitoring/public/directives/cluster_listing/components/cluster_row.jsx
@@ -11,49 +11,101 @@ export default class ClusterRow extends React.Component {
   }
 
   changeCluster() {
+    this.props.changeCluster(this.props.cluster_uuid);
+  }
+
+  handleClickIncompatibleLicense() {
+    return () => {
+      this.props.licenseWarning(
+`You can't view the "${this.props.cluster_name}" cluster because the
+Basic license is not compatible with multi-cluster monitoring.
+
+Need a license? [Get a license with full functionality](https://www.elastic.co/subscriptions/xpack)
+to enjoy multi-cluster monitoring.`
+      );
+    };
+  }
+
+  handleClickInvalidLicense() {
+    return () => {
+      this.props.licenseWarning(
+`You can't view the "${this.props.cluster_name}" cluster because the
+license information is invalid.
+
+Need a license? [Get a free Basic license](https://register.elastic.co/xpack_register)
+or get a license with [full functionality](https://www.elastic.co/subscriptions/xpack)
+to enjoy multi-cluster monitoring.`
+      );
+    };
+  }
+
+  getPrimaryAction() {
     if (this.checkSupported()) {
-      this.props.changeCluster(this.props.cluster_uuid);
+      return (
+        <a className='clusterName' onClick={() => this.changeCluster() }>
+          { this.props.cluster_name } &nbsp;
+          { this.props.isPrimary ?
+            <i className="fa fa-asterisk primary-cluster-indicator" title="Kibana uses this cluster as the primary connection"></i> :
+            '' }
+        </a>
+      );
     }
+
+    // not supported because license is basic/not compatible with multi-cluster
+    if (this.props.license) {
+      return (
+        <a className='clusterName' onClick={this.handleClickIncompatibleLicense()}>{ this.props.cluster_name }</a>
+      );
+    }
+
+    // not supported because license is invalid
+    return (
+      <a className='clusterName' onClick={this.handleClickInvalidLicense()}>{ this.props.cluster_name }</a>
+    );
+  }
+
+  getLicenseInfo() {
+    if (this.props.license) {
+      const licenseExpiry = (() => {
+        if (this.props.license.expiry_date_in_millis < moment().valueOf()) {
+          // license is expired
+          return <div className="expires expired">Expired</div>;
+        }
+
+        // license is fine
+        return (
+          <div className="expires">
+            Expires { moment(this.props.license.expiry_date_in_millis).format('D MMM YY') }
+          </div>
+        );
+      }());
+
+      return (
+        <div>
+          <div className="license">
+            { _.capitalize(this.props.license.type) }
+          </div>
+          { licenseExpiry }
+        </div>
+      );
+    }
+
+    // there is no license!
+    return (
+      <div className="license" onClick={this.handleClickInvalidLicense()}>
+        N/A
+      </div>
+    );
+  }
+
+  /*
+   * helper for avoiding TypeError for nested properties
+   */
+  path(path) {
+    return _.get(this.props, path);
   }
 
   render() {
-
-    const self = this;
-    function get(path) {
-      return _.get(self.props, path);
-    }
-
-
-
-    const licenseInfo = (() => {
-      if (get('license')) {
-        const licenseExpiry = (() => {
-          if (get('license.expiry_date_in_millis') < moment().valueOf()) {
-            // license is expired
-            return <div className="expires expired">Expired</div>;
-          }
-
-          // license is fine
-          return (
-            <div className="expires">
-              Expires { moment(get('license.expiry_date_in_millis')).format('D MMM YY') }
-            </div>
-          );
-        }());
-
-        return (
-          <div>
-            <div className="license">{ _.capitalize(get('license.type')) }</div>
-            { licenseExpiry }
-          </div>
-        );
-      }
-
-      // there is no license!
-      return (
-        <div className="license">None</div>
-      );
-    }());
 
     const classes = ['big'];
     const isSupported = this.checkSupported();
@@ -61,33 +113,40 @@ export default class ClusterRow extends React.Component {
       classes.push('basic');
     }
 
-    const iconClass = statusIconClass(get('status'));
+    const iconClass = statusIconClass(this.props.status);
     return (
       <tr className={ classes.join(' ') }>
         <td key="Name">
-          <a className='clusterName' onClick={(event) => this.changeCluster(event) }>
-            { get('cluster_name') }
-            &nbsp;
-            { get('isPrimary') ?
-              <i className="fa fa-asterisk primary-cluster-indicator" title="Kibana uses this cluster as the primary connection"></i> :
-              '' }
-          </a>
+          { this.getPrimaryAction() }
         </td>
         <td key="Status">
           { isSupported ?
-          <span className={`status status-${get('status')}`}>
-            <i className={iconClass} title={_.capitalize(this.props.status)}></i>
-          </span> : '-' }
+            <span className={`status status-${this.props.status}`}>
+              <i className={iconClass} title={_.capitalize(this.props.status)}></i>
+            </span> : '-' }
         </td>
-        <td key="Nodes">{ isSupported ? numeral(get('elasticsearch.stats.nodes.count.total')).format('0,0') : '-' }</td>
-        <td key="Indices">{ isSupported ? numeral(get('elasticsearch.stats.indices.count')).format('0,0') : '-' }</td>
-        <td key="Data">{ isSupported ? numeral(get('elasticsearch.stats.indices.store.size_in_bytes')).format('0,0[.]0 b') : '-' }</td>
-        <td key="Kibana">{ isSupported ? numeral(get('kibana.count')).format('0,0') : '-' }</td>
+        <td key="Nodes">
+          { isSupported ?
+            numeral(this.path('elasticsearch.stats.nodes.count.total')).format('0,0') :
+            '-' }
+        </td>
+        <td key="Indices">{ isSupported ? numeral(this.path('elasticsearch.stats.indices.count')).format('0,0') : '-' }</td>
+        <td key="Data">
+          { isSupported ?
+            numeral(this.path('elasticsearch.stats.indices.store.size_in_bytes')).format('0,0[.]0 b') :
+            '-' }
+        </td>
+        <td key="Kibana">
+          { isSupported ?
+            numeral(this.path('kibana.count')).format('0,0') :
+            '-' }
+        </td>
         <td key="License" className="license">
-          { licenseInfo }
+          { this.getLicenseInfo() }
         </td>
       </tr>
     );
+
   }
 
 }
