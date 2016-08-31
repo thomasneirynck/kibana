ffe0104cd03de05015aa632511924a5643d05322
Thomas Neirynck <thomas@elastic.co>
2016-08-31 14:55:22 -0400

    fix anchor and text API tests

--- src/core_plugins/tag_cloud/public/vis/components/utils/valuator.js
+++ src/core_plugins/tag_cloud/public/vis/components/utils/valuator.js
@@ -2,11 +2,15 @@ import d3 from 'd3';
 import _ from 'lodash';
 
 function valuator(v) {
-  if (_.isFunction(v)) { return v; }
+  if (_.isFunction(v)) {
+    return v;
+  }
   if (_.isString(v) || _.isNumber(v)) {
-    return function (d) { return d[v]; };
+    return function () {
+      return v;
+    };
   }
   return d3.functor(v);
-};
+}
 
 export default valuator;
