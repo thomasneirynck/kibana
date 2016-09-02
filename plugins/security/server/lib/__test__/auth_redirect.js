18a535cecd999e12bc06ccd163cf0e00635e7b32
Lukas Olson <olson.lukas@gmail.com>
2016-09-02 10:29:08 -0700

    [security-ui] Fix test
    Original commit: elastic/x-pack@7f5cc6ca18cb4ef28a48b636e7e09857adc15ef4

--- plugins/security/server/lib/__test__/auth_redirect.js
+++ plugins/security/server/lib/__test__/auth_redirect.js
@@ -83,7 +83,6 @@ describe('lib/auth_redirect', function () {
         it ('replies with no credentials', () => {
           authenticate(request, reply);
           sinon.assert.calledWith(reply.continue, { credentials: {} });
-          sinon.assert.calledWith(reply.unstate, 'user');
         });
       });
     });
