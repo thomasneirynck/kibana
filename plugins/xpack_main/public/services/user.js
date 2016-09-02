15487b97ab15acfa14840db041ce55f5e837ca96
Lukas Olson <olson.lukas@gmail.com>
2016-09-02 15:27:12 -0700

    [security-ui] Remove unused code
    Original commit: elastic/x-pack@1a92580f3dae80fea79a289fc9ef9ffa884f2a09

--- plugins/xpack_main/public/services/user.js
+++
@@ -1,9 +0,0 @@
-export default function UserProvider($injector, $q) {
-  return {
-    getCurrent() {
-      if (!$injector.has('ShieldUser')) return $q.reject(new Error('Security services are not available'));
-      const ShieldUser = $injector.get('ShieldUser');
-      return ShieldUser.getCurrent();
-    }
-  };
-};
