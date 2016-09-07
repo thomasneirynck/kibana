6d3a82ef33a754b7ddceedd46b81138bba44457d
Joe Fleming <joe.fleming@gmail.com>
2016-09-07 14:09:08 -0700

    make time check test even less strict
    Original commit: elastic/x-pack@37156987fbae362d294e4d98c08d0053a3335b9f

--- plugins/reporting/__test__/server/lib/get_absolute_time.js
+++ plugins/reporting/__test__/server/lib/get_absolute_time.js
@@ -112,7 +112,7 @@ describe('get_absolute_time', function () {
       const output = getAbsoluteTime(timeObj);
       const check = {
         from: /2016\-06\-26T..\:00\:00\.000Z/,
-        to: /2016\-07\-03T..\:59\:59\.999Z/,
+        to: /2016\-07\-0(2|3)T..\:59\:59\.999Z/,
       };
 
       expect(moment(output.from).toISOString()).to.match(check.from);
