f7ebdb37a3867328f37e8c6c9edd65ee4bca1e48
Thomas Neirynck <thomas@elastic.co>
2016-09-08 17:46:04 -0400

    minor edits to arc behaviour /styling

--- src/core_plugins/tag_cloud/public/arcinput_directive.js
+++ src/core_plugins/tag_cloud/public/arcinput_directive.js
@@ -6,9 +6,12 @@ const module = uiModules.get('kibana/tagcloud', ['kibana']);
 module.directive('arcInput', function ($timeout) {
 
   return {
-    template: '<div style="width: 100%; height: 60px"></div>',
+    template: '<div style="width: 100%; height: 100px; margin: 0 auto"></div>',
     link: function (scope, element) {
-      const arcInput = new ArcInput(element[0].firstChild, {});
+      const arcInput = new ArcInput(element[0].firstChild, {
+        sectorFillStyle: 'rgba(255,255,255,1)',
+        sectorStrokeWidth: 3
+      });
 
       arcInput.setMinDegrees(scope.vis.params.fromDegree);
       arcInput.setMaxDegrees(scope.vis.params.toDegree);
