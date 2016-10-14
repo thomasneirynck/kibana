471fe343f58af5fdb3de8861dc59306037399f06
Thomas Neirynck <thomas@elastic.co>
2016-10-14 02:08:27 -0400

    remove unnecessary getters/setters, flatten code

--- src/core_plugins/tag_cloud/public/vis/tag_cloud.js
+++ src/core_plugins/tag_cloud/public/vis/tag_cloud.js
@@ -7,248 +7,124 @@ import textElement from 'plugins/tagcloud/vis/components/elements/text';
 import valuator from 'plugins/tagcloud/vis/components/utils/valuator';
 import vislibComponentsSeedColorsProvider from 'ui/vislib/components/color/seed_colors';
 
-function makeTagCloud() {
 
-  console.log('tagcloud module');
+class TagCloud {
 
-  let textScale = d3.scale.linear();
-  let accessor = function (d) {
-    return d;
-  };
-  let colorScale = d3.scale.ordinal().range(vislibComponentsSeedColorsProvider());
-  let fontNormal = d3.functor('normal');
-  let width = 250;
-  let height = 250;
-  let rotationScale = d3.scale.linear();
-  let orientations = 2;
-  let fromDegree = 0;
-  let toDegree = 0;
-  let font = d3.functor('serif');
-  let fontSize = function (d) {
-    return textScale(d.size);
-  };
-  let fontStyle = fontNormal;
-  let fontWeight = fontNormal;
-  let minFontSize = 12;
-  let maxFontSize = 60;
-  let padding = 1;
-  let textAccessor = function (d) {
-    return d.text;
-  };
-  let fill = function (d) {
-    return colorScale(d.text);
-  };
-  let fillOpacity = d3.functor(1);
-  let textAnchor = d3.functor('middle');
-  let textClass = 'tag';
-
-  function getSize(d) {
-    return d.size;
+  constructor() {
+    this.textScale = d3.scale.linear();
+    this.accessor = function (d) {
+      return d;
+    };
+    const colorScale = d3.scale.ordinal().range(vislibComponentsSeedColorsProvider());
+    this.fontNormal = d3.functor('normal');
+    this.width = 250;
+    this.height = 250;
+    this.rotationScale = d3.scale.linear();
+    this.orientations = 2;
+    this.fromDegree = 0;
+    this.toDegree = 0;
+    this.font = d3.functor('serif');
+    this.fontSize = (d) => {
+      return this.textScale(d.size);
+    };
+    this.fontStyle = this.fontNormal;
+    this.fontWeight = this.fontNormal;
+    this.minFontSize = 12;
+    this.maxFontSize = 60;
+    this.padding = 1;
+    this.textAccessor = function (d) {
+      return d.text;
+    };
+    this.fill = function (d) {
+      return colorScale(d.text);
+    };
+    this.fillOpacity = d3.functor(1);
+    this.textAnchor = d3.functor('middle');
+    this.textClass = 'tag';
   }
 
-  function generator(selection) {
 
-    console.log('make tag cloud', selection);
+  render(selection) {
+
+    const self = this;
     selection.each(function (data, index) {
 
-      let tags = accessor.call(this, data, index);
-      // let tags = data[index].tags;
-      console.log('tags', tags);
+      let tags = self.accessor.call(this, data, index);
 
       let text = textElement()
-        .cssClass(textClass)
-        .fontSize(function (d) {
-          return d.size + 'px';
-        })
-        .fill(fill)
-        .fillOpacity(fillOpacity)
-        .textAnchor(textAnchor);
+        .cssClass(self.textClass)
+        .fontSize(fontSizeAsPixels)
+        .fill(self.fill)
+        .fillOpacity(self.fillOpacity)
+        .textAnchor(self.textAnchor);
 
 
       let group = gGenerator()
         .cssClass('tags')
-        .transform('translate(' + (width > 0 ? width / 2 : 1) + ',' + (height > 0 ? height / 2 : 1) + ')');
+        .transform('translate(' + (self.width > 0 ? self.width / 2 : 1) + ',' + (self.height > 0 ? self.height / 2 : 1) + ')');
 
       let g = d3.select(this)
         .datum([data])
         .call(group);
 
-      let numOfOrientations = orientations - 1;
-      rotationScale
+      let numOfOrientations = self.orientations - 1;
+      self.rotationScale
         .domain([0, numOfOrientations])
-        .range([fromDegree, toDegree]);
+        .range([self.fromDegree, self.toDegree]);
 
-      textScale
+      self.textScale
         .domain(d3.extent(tags, getSize))
-        .range([minFontSize, maxFontSize]);
+        .range([self.minFontSize, self.maxFontSize]);
 
       function draw(tags) {
-        console.log('tags', tags);
         g.select('g.' + group.cssClass())
           .datum(tags)
           .call(text);
       }
 
       layoutCloud()
-        .size([width, height])
+        .size([self.width, self.height])
         .words(tags)
-        .text(textAccessor)
+        .text(self.textAccessor)
         .rotate(function () {
-          return rotationScale(Math.round(Math.random() * numOfOrientations));
+          return self.rotationScale(Math.round(Math.random() * numOfOrientations));
         })
-        .font(font)
-        .fontStyle(fontStyle)
-        .fontWeight(fontWeight)
-        .fontSize(fontSize)
-        .padding(padding)
+        .font(self.font)
+        .fontStyle(self.fontStyle)
+        .fontWeight(self.fontWeight)
+        .fontSize(self.fontSize)
+        .padding(self.padding)
         .on('end', draw)
         .start();
     });
   }
 
-  // Public API
-  generator.accessor = function (v) {
-    if (!arguments.length) {
-      return accessor;
-    }
-    accessor = valuator(v);
-    return generator;
-  };
 
-  generator.width = function (v) {
-
-    if (!arguments.length) {
-      return width;
-    }
-    console.log('set width', v);
-    if (!v) {
-      return generator;
-    }
-    width = v;
-    return generator;
-  };
-
-  generator.height = function (v) {
-    if (!arguments.length) {
-      return height;
-    }
-    console.log('set height', v);
-    if (!v) {
-      return generator;
-    }
-    height = v;
-    return generator;
-  };
-
-  generator.orientations = function (v) {
-    if (!arguments.length) {
-      return orientations;
-    }
-    orientations = v;
-    return generator;
-  };
-
-  generator.fromDegree = function (v) {
-    if (!arguments.length) {
-      return fromDegree;
-    }
-    fromDegree = v;
-    return generator;
-  };
-
-  generator.toDegree = function (v) {
-    if (!arguments.length) {
-      return toDegree;
-    }
-    toDegree = v;
-    return generator;
-  };
-
-  generator.font = function (v) {
-    if (!arguments.length) {
-      return font;
-    }
-    font = v;
-    return generator;
-  };
-
-  generator.fontSize = function (v) {
-    if (!arguments.length) {
-      return fontSize;
-    }
-    fontSize = v;
-    return generator;
-  };
-
-  generator.fontStyle = function (v) {
-    if (!arguments.length) {
-      return fontStyle;
-    }
-    fontStyle = v;
-    return generator;
-  };
-
-  generator.fontWeight = function (v) {
-    if (!arguments.length) {
-      return fontWeight;
-    }
-    fontWeight = v;
-    return generator;
-  };
-
-  generator.minFontSize = function (v) {
-    if (!arguments.length) {
-      return minFontSize;
-    }
-    minFontSize = v;
-    return generator;
-  };
-
-  generator.maxFontSize = function (v) {
-    if (!arguments.length) {
-      return maxFontSize;
-    }
-    maxFontSize = v;
-    return generator;
-  };
-
-  generator.text = function (v) {
-    if (!arguments.length) {
-      return textAccessor;
-    }
-    textAccessor = v;
-    return generator;
-  };
-
-  generator.textScale = function (v) {
-    let scales = ['linear', 'log', 'sqrt'];
-    if (!arguments.length) {
-      return textScale;
-    }
-    textScale = _.includes(scales, v) ? d3.scale[v]() : textScale;
-    return generator;
-  };
-
-
-  generator.textAnchor = function (v) {
-    if (!arguments.length) {
-      return textAnchor;
-    }
-    textAnchor = v;
-    return generator;
-  };
-
-  generator.setOptions = function (options) {
+  setOptions(options) {
     for (let key in options) {//safe prop loop, options is always simple json
       if (options.hasOwnProperty(key)) {
-        generator[key](options[key]);
+
+        if (key === 'textScale') {
+          this[key] = d3.scale[options[key]]();
+        } else {
+          this[key] = options[key];
+        }
+
+
       }
     }
   };
 
-  return generator;
+
 }
 
+function getSize(d) {
+  return d.size;
+}
+
+function fontSizeAsPixels(d) {
+  return d.size + 'px';
+}
 
 /**
  * Renders a tagcloud for each data-set.
@@ -281,13 +157,13 @@ export default class TagCloudVisualization {
       charts.each(function (data) {
 
         const accessor = 'tags';
-        const tagCloud = makeTagCloud()
-          .width(data.width)
-          .height(data.height)
-          .accessor(accessor);
+        const tagCloud = new TagCloud();
+        tagCloud.width = data.width;
+        tagCloud.height = data.height;
+        tagCloud.accessor = valuator(accessor);
 
         tagCloud.setOptions(self._opts);
-        tagCloud(d3.select(this));
+        tagCloud.render(d3.select(this));
       });
 
     });
