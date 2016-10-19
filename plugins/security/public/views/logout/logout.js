aa3e8730f9d09be96f46d555285e5e8ac9fd8120
Lukas Olson <olson.lukas@gmail.com>
2016-10-19 10:59:50 -0700

    [security-ui] Clear session storage on logout (elastic/x-pack#3785)
    [security-ui] Clear session storage on logout
    
    Original commit: elastic/x-pack@cf7ce2c634ef7c2a82f9e87814a23e1023b0a99a

--- plugins/security/public/views/logout/logout.js
+++ plugins/security/public/views/logout/logout.js
@@ -4,6 +4,7 @@ import 'plugins/security/views/logout/logout.less';
 chrome
 .setVisible(false)
 .setRootController('logout', ($http, $window) => {
+  $window.sessionStorage.clear();
   const url = `./login${$window.location.search}`;
   $http.post('./api/security/v1/logout', {}).then(
     () => $window.location.href = url
