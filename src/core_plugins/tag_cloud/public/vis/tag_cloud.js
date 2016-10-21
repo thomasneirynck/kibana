32c205b35bcee6d824ce43c2ca01d94c8ccfae31
Thomas Neirynck <thomas@elastic.co>
2016-10-21 19:10:58 -0400

    start to make this actually work with settings

--- src/core_plugins/tag_cloud/public/vis/tag_cloud.js
+++ src/core_plugins/tag_cloud/public/vis/tag_cloud.js
@@ -1,25 +1,45 @@
 import d3 from 'd3';
 import d3TagCloud from 'd3-cloud';
 
+
 export default class TagCloud {
 
   constructor(element, size) {
-    window.TC = this;
+
     this._element = element;
     this._d3SvgContainer = d3.select(element);
     this._svgGroup = this._d3SvgContainer.append('g');
-    this._size = [1, 1];//initialize array
+
+    this._textScale = 'linear';
+
+    this._fontFamily = 'Impact';
+    this._fontStyle = 'normal';
+    this._fontWeight = 'normal';
+
+    this._orientations = 'single';
+    this._minFontSize = 10;
+    this._maxFontSize = 18;
+
+
+    this._size = [1, 1];
     this.setSize(size);
   }
 
-  /**
-   * @param options
-   */
   setOptions(options) {
     if (JSON.stringify(options) === JSON.stringify(this._options)) {
       return;
     }
-    this._options = options;
+
+    this._options = options;//keep for backup
+    this._fontStyle = options.fontStyle;
+    this._fontWeight = options.fontWeight;
+    this._minFontSize = options.minFontSize;
+    this._maxFontSize = options.maxFontSize;
+    this._textScale = options.textScale;
+    this._orientations = options.orientations;
+
+    console.log(this);
+
     this.invalidate();
   }
 
@@ -31,7 +51,9 @@ export default class TagCloud {
     const enterSelection = stage.enter();
     const enteringTags = enterSelection.append('text');
     enteringTags.style('font-size', getSizeInPixels);
-    enteringTags.style('font-family', 'Impact');
+    enteringTags.style('font-style', this._fontStyle);
+    enteringTags.style('font-weight', this._fontWeight);
+    enteringTags.style('font-family', this._fontFamily);
     enteringTags.style('fill', getFill);
     enteringTags.attr('text-anchor', 'middle');
     enteringTags.attr('transform', positionWord);
@@ -54,7 +76,6 @@ export default class TagCloud {
     let moves = 0;
     const checkIfDone = () => {
       if (exits === 0 && moves === 0) {
-        this._viewDirtyPromise = null;
         resolve(true);
       }
     };
@@ -91,65 +112,52 @@ export default class TagCloud {
     this._words = data.map((tag) => {
       return {text: tag.text, size: 10 + Math.random() * 90};
     });
-    // console.log('words', this._words);
-
     this.invalidate();
   }
 
   destroy() {
-    if (this._viewDirtyPromise) {
-      //wait until ready.
-    }
+    //need to interrupt any ongoing rendering.
     this._element.innerHTML = '';
   }
 
   invalidate() {
+
     if (!this._words) {
       return;
     }
-    this._render().then(_ => {
-      //fire complete event here!
-    });
+
+    clearTimeout(this._timeoutHandle);
+    this._timeoutHandle = setTimeout(_ => {
+      this._render().then(_ => console.log('done.. requires some massaging to deal with multiple renderings..'));
+    }, 0);
+
   }
 
   _render() {
 
-    const render = () => {
-      this._viewDirtyPromise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-        const layout = d3TagCloud();
-        layout.size(this._size);
-        layout.padding(5);
-        layout.rotate(function () {
-          return ~~(Math.random() * 2) * 90;
-        });
-        layout.font(this._font);
-        layout.fontSize(getSize);
-        layout.on('end', this._onLayoutEnd.bind(this, resolve, reject));
-        layout.words(this._words);
-
-        this._d3SvgContainer.attr('width', this._size[0]);
-        this._d3SvgContainer.attr('height', this._size[1]);
-        this._svgGroup.attr('transform', 'translate(' + this._size[0] / 2 + ',' + this._size[1] / 2 + ')');
-
-        layout.start();
-
+      const layout = d3TagCloud();
+      layout.size(this._size);
+      layout.padding(5);
+      layout.rotate(function () {
+        return ~~(Math.random() * 2) * 90;
       });
-    };
+      layout.font(this._fontFamily);
+      layout.fontStyle(this._fontStyle);
+      layout.fontWeight(this._fontWeight);
+      layout.fontSize(getSize);
+      layout.on('end', this._onLayoutEnd.bind(this, resolve, reject));
+      layout.words(this._words);
 
-    // if (this._viewDirtyPromise) {
-    //   this._viewDirtyPromise.then(render);
-    // } else {
-    //   render();
-    // }
-    //
-    // return this._viewDirtyPromise;
+      this._d3SvgContainer.attr('width', this._size[0]);
+      this._d3SvgContainer.attr('height', this._size[1]);
+      this._svgGroup.attr('transform', 'translate(' + this._size[0] / 2 + ',' + this._size[1] / 2 + ')');
 
-    render();
-    return this._viewDirtyPromise;
+      layout.start();
 
+    });
   }
-
 
 };
 
@@ -174,3 +182,15 @@ function getFill(tag) {
   // return tag.fill;
   return 'red';
 }
+
+const ORIENTATIONS = {
+  'single': function () {
+    return 0;
+  },
+  'rightAngled': function () {
+    return ~~(Math.random() * 2) * 90;//random rotation for words
+  },
+  'multi': function () {
+    return ~~(Math.random() * 2 * 6) * 15;//random rotation for words
+  }
+};
