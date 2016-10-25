2e04aaa2c69a1b2063ceeea1fdaf42f936072fdf
spalger <email@spalger.com>
2016-10-25 07:44:02 -0700

    [ui/main] init xpackInfo before toggling nav link
    Original commit: elastic/x-pack@34353fae71175617a6620e4cc3f3e4590cdc9175

--- plugins/graph/public/hacks/toggle_app_link_in_nav.js
+++ plugins/graph/public/hacks/toggle_app_link_in_nav.js
@@ -1,18 +1,18 @@
-import chrome from 'ui/chrome';
 import XPackInfoProvider from 'plugins/xpack_main/services/xpack_info';
-import uiModules from 'ui/modules';
+import chrome from 'ui/chrome';
+import uiRoutes from 'ui/routes';
 
-function toggleAppLinkInNav(Private) {
+uiRoutes.addSetupWork((Private) => {
   const xpackInfo = Private(XPackInfoProvider);
+  return xpackInfo.init().then(() => {
+    const navLink = chrome.getNavLinkById('graph');
+    if (!navLink) return;
 
-  const navLink = chrome.getNavLinkById('graph');
-
-  const showAppLink = xpackInfo.get('features.graph.showAppLink', false);
-  navLink.hidden = !showAppLink;
-  if (showAppLink) {
-    navLink.disabled = !xpackInfo.get('features.graph.enableAppLink', false);
-    navLink.tooltip = xpackInfo.get('features.graph.message');
-  }
-}
-
-uiModules.get('kibana').run(toggleAppLinkInNav);
+    const showAppLink = xpackInfo.get('features.graph.showAppLink', false);
+    navLink.hidden = !showAppLink;
+    if (showAppLink) {
+      navLink.disabled = !xpackInfo.get('features.graph.enableAppLink', false);
+      navLink.tooltip = xpackInfo.get('features.graph.message');
+    }
+  });
+});
