502d611db9dc7b69fc5f7cf2270ab57c13222613
Thomas Neirynck <thomas@elastic.co>
2016-10-28 16:45:14 -0400

    setup boilerplate

---
+++ src/core_plugins/som/public/som.js
@@ -0,0 +1,51 @@
+import 'plugins/som/som.less';
+import 'plugins/som/som_controller.js';
+import 'plugins/som/som_directive.js';
+import TemplateVisTypeTemplateVisTypeProvider from 'ui/template_vis_type/template_vis_type';
+import VisSchemasProvider from 'ui/vis/schemas';
+import somTemplate from 'plugins/som/som.html';
+import somParamTemplate from 'plugins/som/som_params.html';
+import visTypes from 'ui/registry/vis_types';
+
+visTypes.register(SomProvider);
+
+export default function SomProvider(Private) {
+
+  const TemplateVisType = Private(TemplateVisTypeTemplateVisTypeProvider);
+  const Schemas = Private(VisSchemasProvider);
+
+  return new TemplateVisType({
+    name: 'som',
+    title: 'Self Organizing Map',
+    description: 'som description here',
+    icon: 'fa-map-o',
+    template: somTemplate,
+    params: {
+      defaults: {
+      },
+      editor: somParamTemplate
+    },
+    schemas: new Schemas([
+      {
+        group: 'metrics',
+        name: 'metric',
+        title: 'foobar',
+        min: 1,
+        max: 1,
+        aggFilter: ['avg', 'sum', 'count', 'min', 'max', 'median', 'cardinality'],
+        defaults: [
+          { schema: 'metric', type: 'count' }
+        ]
+      },
+      {
+        group: 'buckets',
+        name: 'segment',
+        icon: 'fa fa-map-o',
+        title: 'Tags',
+        min: 1,
+        max: 1,
+        aggFilter: ['terms', 'significant_terms']
+      }
+    ])
+  });
+};
---
+++ src/core_plugins/som/public/som_controller.js
@@ -0,0 +1,10 @@
+import uiModules from 'ui/modules';
+
+const module = uiModules.get('kibana/som', ['kibana']);
+module.controller('KbnSomController', function ($scope) {
+
+  $scope.$watch('esResponse', function (response) {
+    console.log('som controller responding!', response);
+  });
+});
+
