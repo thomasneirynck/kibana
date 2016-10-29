3c16195cd77e514d701d40840b07fc83f8c7351a
Thomas Neirynck <thomas@elastic.co>
2016-10-29 00:31:56 -0400

    it's alive!

--- src/core_plugins/som/public/som_directive.js
+++ src/core_plugins/som/public/som_directive.js
@@ -14,11 +14,60 @@ module.directive('kbnSom', function () {
     template: somDirective,
     replace: 'true',
     link: function (scope, element) {
+
       console.log('som directive ready!', arguments);
 
-      const table = new Table();
 
-      console.log('table', table);
+      let somApp;
+
+      scope.$watch('data', function () {
+        const ponder = window.PONDER;
+
+        if (somApp){
+          somApp.destroy();
+        }
+        
+        if (!scope.data) {
+          return;
+        }
+
+        const table = new Table(scope.data);
+        console.log('table', table);
+
+
+        window.table = table;
+
+         somApp = ponder.createSOM({
+          table: table,
+          nodes: {
+            toolbar: "mapToolContainer",
+            mapTableToggle: "toggle",
+            table: "tableContainer",
+            map: "map",
+            toggleToMap: "toggle-to-map",
+            toggleToTable: "toggle-to-table",
+            container: element[0],
+            center: "center",
+            waiting: "waiting"
+          },
+          bmu: {
+            initialColumn: null //?bwerugh?
+          }
+        });
+
+
+        somApp.on("AppLoaded", function () {
+          //fuck da intro!
+          console.log('loaded');
+        });
+
+        console.log(somApp);
+        window.somApp = somApp;
+
+
+      });
+
+
     }
   };
 });
