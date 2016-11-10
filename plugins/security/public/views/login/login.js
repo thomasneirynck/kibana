62e648e7076c2a57e298247139e97df951fd5529
Shaunak Kashyap <ycombinator@gmail.com>
2016-11-11 04:17:34 +0530

    Use new login_state API and service on login page
    Original commit: elastic/x-pack@0195e0c962a1b174cdf6824b97f9b5ced0c214d1

--- plugins/security/public/views/login/login.js
+++ plugins/security/public/views/login/login.js
@@ -1,11 +1,11 @@
 import { parse } from 'url';
 import { get } from 'lodash';
 import 'ui/autoload/styles';
+import 'plugins/security/services/login_state';
 import 'plugins/security/views/login/login.less';
 import chrome from 'ui/chrome';
 import parseNext from 'plugins/security/lib/parse_next';
 import template from 'plugins/security/views/login/login.html';
-import XPackInfoProvider from 'plugins/xpack_main/services/xpack_info';
 
 const messageMap = {
   SESSION_EXPIRED: 'Your session has expired. Please log in again.'
@@ -14,17 +14,17 @@ const messageMap = {
 chrome
 .setVisible(false)
 .setRootTemplate(template)
-.setRootController('login', function ($http, $window, secureCookies, Private) {
-  const xpackInfo = Private(XPackInfoProvider);
+.setRootController('login', function ($http, $window, secureCookies, loginState) {
   const next = parseNext($window.location);
   const isSecure = !!$window.location.protocol.match(/^https/);
   const self = this;
 
-  function setupScope() {
+  function setupScope({ allowLogin, loginMessage }) {
     const defaultLoginMessage = 'Login is currently disabled because the license could not be determined. '
     + 'Please check that Elasticsearch is running, then refresh this page.';
-    self.allowLogin = xpackInfo.get('features.security.allowLogin', false);
-    self.loginMessage = xpackInfo.get('features.security.loginMessage', defaultLoginMessage);
+
+    self.allowLogin = allowLogin;
+    self.loginMessage = loginMessage || defaultLoginMessage;
     self.infoMessage = get(messageMap, parse($window.location.href, true).query.msg);
     self.isDisabled = !isSecure && secureCookies;
     self.isLoading = false;
@@ -42,6 +42,6 @@ chrome
     };
   }
 
-  xpackInfo.refresh()
-  .finally(setupScope);
+  loginState.get()
+  .then(setupScope);
 });
