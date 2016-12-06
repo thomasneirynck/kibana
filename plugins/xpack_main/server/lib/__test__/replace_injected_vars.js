55b1470e7e119bb7e7b2e78bf8782e75fbef7c3e
Court Ewing <court@epixa.com>
2016-12-05 21:41:18 -0500

    security: verify non-basic license for var injection (elastic/x-pack#4282)
    Since security is suppose to be automatically disabled when the license
    is basic, it's important that we do not attempt to authenticate the
    current user before injecting variables, otherwise we trigger a 403
    error due to the invalid license against the security authenticate API,
    which causes Kibana to fail to start up properly.
    
    Original commit: elastic/x-pack@3f04f1cb9a2689f193d7d8c57db379ee7819caa2

--- plugins/xpack_main/server/lib/__test__/replace_injected_vars.js
+++ plugins/xpack_main/server/lib/__test__/replace_injected_vars.js
@@ -4,21 +4,10 @@ import expect from 'expect.js';
 import replaceInjectedVars from '../replace_injected_vars';
 
 describe('replaceInjectedVars uiExport', () => {
-  it('checks if request is authenticated, sends xpack info if so', async () => {
+  it('sends xpack info if request is authenticated and license is not basic', async () => {
     const originalInjectedVars = { a: 1 };
     const request = {};
-    const server = {
-      plugins: {
-        security: {
-          isAuthenticated: sinon.stub().returns(true)
-        },
-        xpack_main: {
-          info: {
-            toJSON: () => ({ b: 1 })
-          }
-        }
-      }
-    };
+    const server = mockServer();
 
     const newVars = await replaceInjectedVars(originalInjectedVars, request, server);
     expect(newVars).to.eql({
@@ -32,18 +21,87 @@ describe('replaceInjectedVars uiExport', () => {
     expect(server.plugins.security.isAuthenticated.firstCall.args[0]).to.be(request);
   });
 
+  it('sends the xpack info if plugin is disabled', async () => {
+    const originalInjectedVars = { a: 1 };
+    const request = {};
+    const server = mockServer();
+    delete server.plugins.security;
+
+    const newVars = await replaceInjectedVars(originalInjectedVars, request, server);
+    expect(newVars).to.eql({
+      a: 1,
+      xpackInitialInfo: {
+        b: 1
+      }
+    });
+  });
+
+  it('sends the xpack info xpack license is basic', async () => {
+    const originalInjectedVars = { a: 1 };
+    const request = {};
+    const server = mockServer();
+    server.plugins.xpack_main.info.license.isOneOf.returns(true);
+
+    const newVars = await replaceInjectedVars(originalInjectedVars, request, server);
+    expect(newVars).to.eql({
+      a: 1,
+      xpackInitialInfo: {
+        b: 1
+      }
+    });
+  });
+
   it('sends the originalInjectedVars if not authenticated', async () => {
     const originalInjectedVars = { a: 1 };
     const request = {};
-    const server = {
-      plugins: {
-        security: {
-          isAuthenticated: sinon.stub().returns(false)
-        }
-      }
-    };
+    const server = mockServer();
+    server.plugins.security.isAuthenticated.returns(false);
+
+    const newVars = await replaceInjectedVars(originalInjectedVars, request, server);
+    expect(newVars).to.be(originalInjectedVars);
+  });
+
+  it('sends the originalInjectedVars if xpack info is unavailable', async () => {
+    const originalInjectedVars = { a: 1 };
+    const request = {};
+    const server = mockServer();
+    delete server.plugins.xpack_main.info;
+
+    const newVars = await replaceInjectedVars(originalInjectedVars, request, server);
+    expect(newVars).to.be(originalInjectedVars);
+  });
+
+  it('sends the originalInjectedVars if the license check result is not available', async () => {
+    const originalInjectedVars = { a: 1 };
+    const request = {};
+    const server = mockServer();
+    server.plugins.xpack_main.info.feature().getLicenseCheckResults.returns(undefined);
 
     const newVars = await replaceInjectedVars(originalInjectedVars, request, server);
     expect(newVars).to.be(originalInjectedVars);
   });
 });
+
+// creates a mock server object that defaults to being authenticated with a
+// non-basic license
+function mockServer() {
+  const getLicenseCheckResults = sinon.stub().returns({});
+  return {
+    plugins: {
+      security: {
+        isAuthenticated: sinon.stub().returns(true)
+      },
+      xpack_main: {
+        info: {
+          feature: () => ({
+            getLicenseCheckResults
+          }),
+          license: {
+            isOneOf: sinon.stub().returns(false)
+          },
+          toJSON: () => ({ b: 1 })
+        }
+      }
+    }
+  };
+}
