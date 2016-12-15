1b3466c0f07394ccb9341deabc5d42996a605e22
Thomas Neirynck <thomas@elastic.co>
2016-12-15 00:43:46 -0500

    Change Kibana location to reflect new dev-setup.
    The x-plugins need now be deployed in a directory called elasticsearch-extra/x-pack, which is one directory deeper than in the previous setup.
    
    Original commit: elastic/x-pack@e53fe2f09d8292b152e552b57e9bebec732a67e3

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ require('babel/register')();
 require('dotenv').config({ silent: true });
 
 // relative location of Kibana install
-var pathToKibana = process.env.KIBANA_PATH || '../../kibana';
+var pathToKibana = process.env.KIBANA_PATH || '../../../kibana';
 
 var gulp = require('gulp');
 var g = require('gulp-load-plugins')();
