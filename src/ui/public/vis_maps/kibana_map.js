54594ba8510974901a6a7587d1a464f0fa1ae743
Thomas Neirynck <thomas@elastic.co>
2016-12-29 15:38:50 -0500

    Map enhancements
    This is an initial commit of a larger  effort for improving the maps in Kibana. It will include:
    - addition of vector map visualization (choropleth maps).
    - various enhancements to the existing TileMap to improve usability and performance.
    
    Another goal is also to hugely reduce the code footprint of the current tilemap implementation.

---
+++ src/ui/public/vis_maps/kibana_map.js
@@ -0,0 +1,556 @@
+import { EventEmitter } from 'events';
+import L from 'leaflet';
+import _ from 'lodash';
+import d3 from 'd3';
+
+import Notifier from 'ui/notify/notifier';
+
+
+class GeohashGridOverlay {
+
+  constructor(featureCollection, layerOptions) {
+    this._featureCollection = featureCollection;
+    this.createLeafletLayer(layerOptions);
+  }
+
+  createLeafletLayer(ignoreLayerOptions) {
+    const min = _.get(this._featureCollection, 'properties.min', 0);
+    const max = _.get(this._featureCollection, 'properties.max', 1);
+    this._legendColors = makeLegendColors(min, max);
+    this._leafletLayer = new L.geoJson(null, {
+      pointToLayer: this.getMarkerFunction(),
+      style: makeStyleFunction(min, max, this._legendColors)
+    });
+    this._leafletLayer.addData(this._featureCollection);
+  }
+
+  getFeatureCollection() {
+    return this._featureCollection;
+  }
+
+  addToLeafletMap(leafletMap) {
+    leafletMap.invalidateSize();
+    this._leafletLayer.addTo(leafletMap);
+  }
+
+  removeFromMap(leafletMap) {
+    leafletMap.removeLayer(this._leafletLayer);
+  }
+
+  getMarkerFunction() {
+    throw new Error('should implement getMarkerFunction');
+  }
+
+}
+
+
+class ScaledCircleOverlay extends GeohashGridOverlay {
+  constructor() {
+    super(...arguments);
+  }
+
+  getMarkerFunction() {
+    const scaleFactor = 0.6;
+    return (feature, latlng) => {
+      const value = feature.properties.value;
+      const scaledRadius = this._radiusScale(value) * scaleFactor;
+      return L.circleMarker(latlng).setRadius(scaledRadius);
+    }
+  }
+
+  /**
+   * radiusScale returns a number for scaled circle markers
+   * for relative sizing of markers
+   *
+   * @method _radiusScale
+   * @param value {Number}
+   * @return {Number}
+   */
+  _radiusScale(value) {
+    const precisionBiasBase = 5;
+    const precisionBiasNumerator = 200;
+    const zoom = this._featureCollection.properties.zoom;
+    const maxValue = this._featureCollection.properties.max;
+    const precision = _.max(this._featureCollection.features.map((feature) => {
+      return String(feature.properties.geohash).length;
+    }));
+
+    const pct = Math.abs(value) / Math.abs(maxValue);
+    const zoomRadius = 0.5 * Math.pow(2, zoom);
+    const precisionScale = precisionBiasNumerator / Math.pow(precisionBiasBase, precision);
+
+    // square root value percentage
+    return Math.pow(pct, 0.5) * zoomRadius * precisionScale;
+  }
+
+
+}
+
+
+/**
+ * Map overlay: canvas layer with leaflet.heat plugin
+ *
+ * @param map {Leaflet Object}
+ * @param geoJson {geoJson Object}
+ * @param params {Object}
+ */
+class HeatmapOverlay extends GeohashGridOverlay {
+
+  constructor(featureCollection, layerOptions) {
+    super(...arguments);
+    // super(map, geoJson, params);
+    // this._disableTooltips = false;
+    //
+    // this._createMarkerGroup({
+    //   radius: +this._attr.heatRadius,
+    //   blur: +this._attr.heatBlur,
+    //   maxZoom: +this._attr.heatMaxZoom,
+    //   minOpacity: +this._attr.heatMinOpacity
+    // });
+    //
+    // this.addLegend = _.noop;
+    //
+    // this._getLatLng = _.memoize(function (feature) {
+    //   return L.latLng(
+    //     feature.geometry.coordinates[1],
+    //     feature.geometry.coordinates[0]
+    //   );
+    // }, function (feature) {
+    //   // turn coords into a string for the memoize cache
+    //   return [feature.geometry.coordinates[1], feature.geometry.coordinates[0]].join(',');
+    // });
+  }
+
+
+  createLeafletLayer(heatmapOptions) {
+    const max = _.get(this._featureCollection, 'properties.max');
+    const points = dataToHeatArray(max, heatmapOptions.heatNormalizeData, this._featureCollection);
+    this._leafletLayer = L.heatLayer(points, heatmapOptions);
+    // this._fixTooltips();
+    // this._addToMap();
+  }
+
+  // _createMarkerGroup(options) {
+  //   const max = _.get(this.geoJson, 'properties.allmax');
+  //   const points = this._dataToHeatArray(max);
+  //
+  //   this._markerGroup = L.heatLayer(points, options);
+  //   this._fixTooltips();
+  //   this._addToMap();
+  // }
+  //
+  // _fixTooltips() {
+  //   const self = this;
+  //   const debouncedMouseMoveLocation = _.debounce(mouseMoveLocation.bind(this), 15, {
+  //     'leading': true,
+  //     'trailing': false
+  //   });
+  //
+  //   if (!this._disableTooltips && this._attr.addTooltip) {
+  //     this.map.on('mousemove', debouncedMouseMoveLocation);
+  //     this.map.on('mouseout', function () {
+  //       self.map.closePopup();
+  //     });
+  //     this.map.on('mousedown', function () {
+  //       self._disableTooltips = true;
+  //       self.map.closePopup();
+  //     });
+  //     this.map.on('mouseup', function () {
+  //       self._disableTooltips = false;
+  //     });
+  //   }
+  //
+  //   function mouseMoveLocation(e) {
+  //     const latlng = e.latlng;
+  //
+  //     this.map.closePopup();
+  //
+  //     // unhighlight all svgs
+  //     d3.selectAll('path.geohash', this.chartEl).classed('geohash-hover', false);
+  //
+  //     if (!this.geoJson.features.length || this._disableTooltips) {
+  //       return;
+  //     }
+  //
+  //     // find nearest feature to event latlng
+  //     const feature = this._nearestFeature(latlng);
+  //
+  //     // show tooltip if close enough to event latlng
+  //     if (this._tooltipProximity(latlng, feature)) {
+  //       this._showTooltip(feature, latlng);
+  //     }
+  //   }
+  // }
+  //
+  // /**
+  //  * Finds nearest feature in mapData to event latlng
+  //  *
+  //  * @method _nearestFeature
+  //  * @param latLng {Leaflet latLng}
+  //  * @return nearestPoint {Leaflet latLng}
+  //  */
+  // _nearestFeature(latLng) {
+  //   const self = this;
+  //   let nearest;
+  //
+  //   if (latLng.lng < -180 || latLng.lng > 180) {
+  //     return;
+  //   }
+  //
+  //   _.reduce(this.geoJson.features, function (distance, feature) {
+  //     const featureLatLng = self._getLatLng(feature);
+  //     const dist = latLng.distanceTo(featureLatLng);
+  //
+  //     if (dist < distance) {
+  //       nearest = feature;
+  //       return dist;
+  //     }
+  //
+  //     return distance;
+  //   }, Infinity);
+  //
+  //   return nearest;
+  // }
+  //
+  // /**
+  //  * display tooltip if feature is close enough to event latlng
+  //  *
+  //  * @method _tooltipProximity
+  //  * @param latlng {Leaflet latLng  Object}
+  //  * @param feature {geoJson Object}
+  //  * @return {Boolean}
+  //  */
+  // _tooltipProximity(latlng, feature) {
+  //   if (!feature) return;
+  //
+  //   let showTip = false;
+  //   const featureLatLng = this._getLatLng(feature);
+  //
+  //   // zoomScale takes map zoom and returns proximity value for tooltip display
+  //   // domain (input values) is map zoom (min 1 and max 18)
+  //   // range (output values) is distance in meters
+  //   // used to compare proximity of event latlng to feature latlng
+  //   const zoomScale = d3.scale.linear()
+  //     .domain([1, 4, 7, 10, 13, 16, 18])
+  //     .range([1000000, 300000, 100000, 15000, 2000, 150, 50]);
+  //
+  //   const proximity = zoomScale(this.map.getZoom());
+  //   const distance = latlng.distanceTo(featureLatLng);
+  //
+  //   // maxLngDif is max difference in longitudes
+  //   // to prevent feature tooltip from appearing 360°
+  //   // away from event latlng
+  //   const maxLngDif = 40;
+  //   const lngDif = Math.abs(latlng.lng - featureLatLng.lng);
+  //
+  //   if (distance < proximity && lngDif < maxLngDif) {
+  //     showTip = true;
+  //   }
+  //
+  //   d3.scale.pow().exponent(0.2)
+  //     .domain([1, 18])
+  //     .range([1500000, 50]);
+  //   return showTip;
+  // }
+
+}
+
+
+
+
+class KibanaMap extends EventEmitter {
+
+  constructor(domNode) {
+
+    super();
+
+    this._leafletMap = L.map(domNode, {});
+    this._leafletMap.setView([0, 0], 0);//todo: pass in from UI-state (if any)
+    this._leafletMap.on('zoomend', e => this.emit('zoomend'));
+    this._leafletMap.on('moveend', e => this.emit('moveend'));
+
+    this._leafletMap.on('draw:created', event => {
+      const drawType = event.layerType;
+      if (drawType === 'rectangle') {
+        const bounds = event.layer.getBounds();
+
+        const southEast = bounds.getSouthEast();
+        const northWest = bounds.getNorthWest();
+        let southEastLng = southEast.lng;
+        if (southEastLng > 180) {
+          southEastLng -= 360;
+        }
+        let northWestLng = northWest.lng;
+        if (northWestLng < -180) {
+          northWestLng += 360;
+        }
+
+        const southEastLat = southEast.lat;
+        const northWestLat = northWest.lat;
+
+        //Bounds cannot be created unless they form a box with larger than 0 dimensions
+        //Invalid areas are rejected by ES.
+        if (southEastLat === northWestLat || southEastLng === northWestLng) {
+          return;
+        }
+
+        this.emit('drawCreated:rectangle', {
+          bounds: {
+            bottom_right: {
+              lat: southEastLat,
+              lon: southEastLng
+            },
+            top_left: {
+              lat: northWestLat,
+              lon: northWestLng
+            }
+          }
+        });
+      } else if (drawType === 'polygon') {
+        const latLongs = event.layer.getLatLngs();
+        this.emit('drawCreated:polygon', {
+          points: latLongs.map(leafletLatLng => {
+            return {
+              lat: leafletLatLng.lat,
+              lon: leafletLatLng.lng
+            };
+          })
+        });
+      }
+
+
+    });
+
+
+
+
+
+    this.resize();
+
+    this._leafletBaseLayer = null;
+    this._geohashGridOverlay = null;
+    this._leafletDrawControl = null;
+    this._geohashOptions = {};
+
+    this._addDrawControl();
+
+  }
+
+  destroy() {
+    //todo
+  }
+
+
+  getCenter() {
+    const center = this._leafletMap.getCenter();
+    return {lon: center.lng, lat: center.lat};
+  }
+
+  setCenter(latitude, longitude) {
+    const latLong = new L.latLng(latitude, longitude);
+    if (latLong.equals && !latLong.equals(this._leafletMap.getCenter())) {
+      this._leafletMap.setView(latLong);
+    }
+  }
+
+
+  setZoomLevel(zoomLevel) {
+    if (this._leafletMap.getZoom() !== zoomLevel) {
+      this._leafletMap.setZoom(zoomLevel);
+    }
+  }
+
+  getZoomLevel(){
+    return this._leafletMap.getZoom();
+  }
+
+
+  setTMSBaseLayer(options) {
+    if (!this._leafletBaseLayer) {
+      this._leafletBaseLayer = new L.tileLayer(options.url, {});
+      this._leafletBaseLayer.addTo(this._leafletMap);
+      this._leafletBaseLayer.bringToBack();
+      this.resize();
+      return;
+    }
+
+    //todo: make changes if settings change
+    console.log('todo: make changes if settings change');
+    console.log('todo: make changes if settings change');
+
+
+  }
+
+  setWMSBaseLayer(options){
+    console.log('todo: setWMSBaseLayer', arguments);
+
+  }
+
+  _addDrawControl() {
+
+    const drawOptions = {
+      draw: {
+        polyline: false,
+        marker: false,
+        circle: false,
+        polygon: {
+          shapeOptions: {
+            stroke: false,
+            color: '#000'
+          }
+        },
+        rectangle: {
+          shapeOptions: {
+            stroke: false,
+            color: '#000'
+          }
+        }
+      }
+    };
+
+
+    this._leafletDrawControl = new L.Control.Draw(drawOptions);
+    this._leafletMap.addControl(this._leafletDrawControl)
+  };
+
+
+  _recreateOverlay() {
+
+    if (!this._featureCollection) {
+      return;
+    }
+
+    if (this._geohashGridOverlay) {
+      this._geohashGridOverlay.removeFromMap(this._leafletMap);
+    }
+
+    switch (this._geohashOptions.mapType) {
+      case 'Scaled Circle Markers':
+        this._geohashGridOverlay = new ScaledCircleOverlay(this._featureCollection);
+        break;
+      case 'Heatmap':
+        this._geohashGridOverlay = new HeatmapOverlay(this._featureCollection, {
+          radius: this._geohashOptions.heatmap.heatRadius,
+          blur: this._geohashOptions.heatmap.heatBlur,
+          maxZoom: this._geohashOptions.heatmap.heatMaxZoom,
+          minOpaxity: this._geohashOptions.heatmap.heatMinOpacity,
+          heatNormalizeData: this._geohashOptions.heatmap.heatNormalizeData
+        });
+        break;
+      default:
+        throw new Error(`${this._geohashOptions.mapType} mapType not recognized`);
+
+    }
+    this._geohashGridOverlay.addToLeafletMap(this._leafletMap);
+  }
+
+  setGeohashFeatureCollection(featureCollection) {
+    if (this._geohashGridOverlay && _.isEqual(this._geohashGridOverlay.getFeatureCollection(), featureCollection)) {
+      //no need to recreate layer.
+      return;
+    }
+    this._featureCollection = featureCollection;
+    this._recreateOverlay();
+  }
+
+  setGeohashLayerOptions(options) {
+
+    if (_.isEqual(this._geohashOptions, options)) {
+      return;
+    }
+
+    const previousOptions = this._geohashOptions;
+    this._geohashOptions = options;
+
+    //do smart refresh. only required changes.
+    if (previousOptions)
+      if (this._geohashOptions.mapType !== previousOptions.mapType) {
+        this._recreateOverlay();
+      } else if (this._geohashOptions.mapType === 'Heatmap' && !_.isEqual(this._geohashOptions.heatmap, previousOptions)) {
+        this._recreateOverlay();//todo: can be optimized. does not require full refresh
+      }
+
+    //todo: when other configurations change (tooltip, etc... do NOT recreate the layer
+
+  }
+
+  resize() {
+    this._leafletMap.invalidateSize();
+  }
+
+}
+
+
+/**
+ * d3 quantize scale returns a hex color, used for marker fill color
+ *
+ * @method quantizeLegendColors
+ * return {undefined}
+ */
+function makeLegendColors(min, max) {
+  const reds1 = ['#ff6128'];
+  const reds3 = ['#fecc5c', '#fd8d3c', '#e31a1c'];
+  const reds5 = ['#fed976', '#feb24c', '#fd8d3c', '#f03b20', '#bd0026'];
+  const bottomCutoff = 2;
+  const middleCutoff = 24;
+  let legendColors;
+  if (max - min <= bottomCutoff) {
+    legendColors = reds1;
+  } else if (max - min <= middleCutoff) {
+    legendColors = reds3;
+  } else {
+    legendColors = reds5;
+  }
+  return legendColors;
+}
+
+function makeColorDarker(color) {
+  const amount = 1.3;//magic number, carry over from earlier
+  return d3.hcl(color).darker(amount).toString();
+}
+
+function makeStyleFunction(min, max, legendColors) {
+  const quantizeDomain = (min !== max) ? [min, max] : d3.scale.quantize().domain();
+  const legendQuantizer = d3.scale.quantize().domain(quantizeDomain).range(legendColors);
+  return (feature) => {
+    const value = _.get(feature, 'properties.value');
+    const color = legendQuantizer(value);
+    return {
+      fillColor: color,
+      color: makeColorDarker(color),
+      weight: 1.5,
+      opacity: 1,
+      fillOpacity: 0.75
+    };
+  };
+}
+
+/**
+ * returns data for data for heat map intensity
+ * if heatNormalizeData attribute is checked/true
+ • normalizes data for heat map intensity
+ *
+ * @method _dataToHeatArray
+ * @param max {Number}
+ * @return {Array}
+ */
+function dataToHeatArray(max, heatNormalizeData, featureCollection) {
+
+  return featureCollection.features.map((feature) => {
+    const lat = feature.properties.center[0];
+    const lng = feature.properties.center[1];
+    let heatIntensity;
+
+    if (!heatNormalizeData) {
+      // show bucket value on heatmap
+      heatIntensity = feature.properties.value;
+    } else {
+      // show bucket value normalized to max value
+      heatIntensity = feature.properties.value / max;
+    }
+
+    return [lat, lng, heatIntensity];
+  });
+}
+
+export default KibanaMap;
---
+++ src/ui/public/vis_maps/maps_renderbot2.js
@@ -0,0 +1,173 @@
+import _ from 'lodash';
+import MapsProvider from 'ui/vis_maps/maps';//todo: remove include
+import VisRenderbotProvider from 'ui/vis/renderbot';
+import MapsVisTypeBuildChartDataProvider from 'ui/vislib_vis_type/build_chart_data';
+import FilterBarPushFilterProvider from 'ui/filter_bar/push_filter';
+import KibanaMap from './kibana_map';
+import $ from 'jquery';
+
+module.exports = function MapsRenderbotFactory(Private, $injector, tilemapSettings, Notifier, courier, getAppState) {
+
+  const Renderbot = Private(VisRenderbotProvider);
+  const buildChartData = Private(MapsVisTypeBuildChartDataProvider);
+  const notify = new Notifier({
+    location: 'Tilemap'
+  });
+
+
+  class MapsRenderbot extends Renderbot {
+
+    constructor(vis, $el, uiState) {
+      super(vis, $el, uiState);
+
+      this._buildChartData = buildChartData.bind(this);//todo: buildChartData shouldn't be a mixin. too confusing.
+
+
+      if (tilemapSettings.getError()) {
+        //Still allow the visualization to be build, but show a toast that there was a problem retrieving map settings
+        //Even though the basemap will not display, the user will at least still see the overlay data
+        notify.warning(tilemapSettings.getError().message);
+      }
+      const containerElement = $($el)[0];
+
+      this._kibanaMap = new KibanaMap(containerElement);
+      this._kibanaMap.on('moveend', _ => this._persistUIStateFromMap());
+      this._kibanaMap.on('zoomend', _ => {
+          this._persistUIStateFromMap();
+          console.log('todo! this needs to be toggleable with autoPrecision params');
+          courier.fetch();
+        }
+      );
+
+
+      this._kibanaMap.on('drawCreated:rectangle', event => {
+        addSpatialFilter(_.get(this.mapsData, 'geohashGridAgg'), 'geo_bounding_box', event.bounds);
+      });
+
+      this._kibanaMap.on('drawCreated:polygon', event => {
+        addSpatialFilter(_.get(this.mapsData, 'geohashGridAgg'), 'geo_polygon', {points: event.points});
+      });
+
+      
+      this._configureGeoHashLayer();
+      this._useUIState();
+    }
+
+    render(esResponse) {
+      console.log('render, esResponse', esResponse);
+      this.mapsData = this._buildChartData(esResponse);
+      const params = this._getMapsParams();
+      this.mapsParams = params;
+
+      this._kibanaMap.setGeohashFeatureCollection(this.mapsData.geoJson);
+      this._useUIState();
+      this._kibanaMap.resize();
+    }
+
+    destroy() {
+      const mapsVis = this.mapsVis;
+      _.forOwn(this.vis.listeners, function (listener, event) {
+        mapsVis.off(event, listener);
+      });
+      mapsVis.destroy();
+    }
+
+    updateParams() {
+
+      const newParams = this._getMapsParams();
+      this._configureGeoHashLayer();
+
+      if (newParams.wms.enabled) {
+        this._kibanaMap.setWMSBaseLayer({
+          options: newParams.wms.options,
+          url: newParams.wms.url
+        });
+      } else {
+        const url = tilemapSettings.getUrl();
+        const options = tilemapSettings.getTMSOptions();
+        this._kibanaMap.setTMSBaseLayer({url, ...options});
+      }
+
+      this._useUIState();
+
+    }
+
+
+    _useUIState() {
+      const uiState = this.vis.getUiState();
+      const newParams = this._getMapsParams();
+
+      const zoomFromUiState = parseInt(uiState.get('mapZoom'));
+      const centerFromUIState = uiState.get('mapCenter');
+      if (!isNaN(zoomFromUiState)) {
+        this._kibanaMap.setZoomLevel(zoomFromUiState);
+      } else {// this._kibanaMap.setCenter(newParams.mapCenter[0], newParams.mapCenter[1]);
+        this._kibanaMap.setZoomLevel(newParams.mapZoom);
+      }
+      if (centerFromUIState) {
+        this._kibanaMap.setCenter(centerFromUIState[0], centerFromUIState[1]);
+      } else {
+        this._kibanaMap.setCenter(newParams.mapCenter[0], newParams.mapCenter[1]);
+      }
+    }
+
+    _persistUIStateFromMap() {
+
+      const uiState = this.vis.getUiState();
+      const zoomFromUIState = parseInt(uiState.get('mapZoom'));
+      const centerFromUIState = uiState.get('mapCenter');
+      if (isNaN(zoomFromUIState) || this._kibanaMap.getZoomLevel() !== zoomFromUIState) {
+        uiState.set('mapZoom', this._kibanaMap.getZoomLevel());
+      }
+
+      const centerFromMap = this._kibanaMap.getCenter();
+      if (!centerFromUIState || centerFromMap.lon !== centerFromUIState[1] || centerFromMap.lat !== centerFromUIState[0]) {
+        uiState.set('mapCenter', [centerFromMap.lat, centerFromMap.lon]);
+      }
+    }
+
+    _getMapsParams() {
+      return _.assign({}, this.vis.type.params.defaults, {
+          type: this.vis.type.name,
+          // Add attribute which determines whether an index is time based or not.
+          hasTimeField: this.vis.indexPattern && this.vis.indexPattern.hasTimeField()
+        },
+        this.vis.params
+      );
+    }
+
+
+    _configureGeoHashLayer() {
+      const newParams = this._getMapsParams();
+      this._kibanaMap.setGeohashLayerOptions({
+        mapType: newParams.mapType,
+        heatmap: {
+          heatBlur: newParams.heatBlur,
+          heatMaxZoom: newParams.heatMaxZoom,
+          heatMinOpacity: newParams.heatMinOpacity,
+          heatNormalizeData: newParams.heatNormalizeData,
+          heatRadius: newParams.heatRadius
+        }
+      });
+    }
+  }
+
+
+  function addSpatialFilter(agg, filterName, filterData) {
+    if (!agg) {
+      return;
+    }
+    const pushFilter = Private(FilterBarPushFilterProvider)(getAppState());
+    const indexPatternName = agg.vis.indexPattern.id;
+    const field = agg.fieldName();
+    const filter = {};
+    filter[filterName] = {};
+    filter[filterName][field] = filterData;
+    pushFilter(filter, false, indexPatternName);
+  }
+
+
+  return MapsRenderbot;
+};
+
+
