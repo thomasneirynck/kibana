07013c79a69472a4e9247e3eeb133cbc00b6b262
Stacey Gammon <gammon@elastic.co>
2017-01-24 15:19:45 -0500

    Use a constant to make this less brittle to a changing url in the main repo
    Original commit: elastic/x-pack@7eca6db9545e62a36a74263a3fb3b86e230470c1

--- plugins/reporting/public/controls/dashboard.js
+++ plugins/reporting/public/controls/dashboard.js
@@ -1,6 +1,7 @@
 import 'plugins/reporting/directives/export_config';
 import XPackInfoProvider from 'plugins/xpack_main/services/xpack_info';
 import navbarExtensions from 'ui/registry/navbar_extensions';
+import { DashboardConstants } from 'plugins/kibana/dashboard/dashboard_constants';
 
 function dashboardReportProvider(Private, $location) {
   const xpackInfo = Private(XPackInfoProvider);
@@ -11,7 +12,7 @@ function dashboardReportProvider(Private, $location) {
     label: 'Reporting',
     template: '<export-config object-type="Dashboard"></export-config>',
     description: 'Dashboard Report',
-    hideButton: () => $location.path().endsWith('/dashboard') || !xpackInfo.get('features.reporting.showLinks', false),
+    hideButton: () => $location.path() === DashboardConstants.LANDING_PAGE_URL || !xpackInfo.get('features.reporting.showLinks', false),
     disableButton: () => !xpackInfo.get('features.reporting.enableLinks', false),
     tooltip: () => xpackInfo.get('features.reporting.message')
   };
