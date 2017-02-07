60a9f3a8aee2161432a96b2eb9e7aef45a05dff6
CJ Cenizal <cj@cenizal.com>
2017-02-06 18:47:59 -0800

    Hide Reporting link on Visualize listing page. (elastic/x-pack#4799)
    Original commit: elastic/x-pack@8b05ae3623f10f6a8f13ffff7a9ed41b63278dcb

--- plugins/reporting/public/controls/visualize.js
+++ plugins/reporting/public/controls/visualize.js
@@ -1,8 +1,9 @@
 import 'plugins/reporting/directives/export_config';
 import XPackInfoProvider from 'plugins/xpack_main/services/xpack_info';
 import navbarExtensions from 'ui/registry/navbar_extensions';
+import { VisualizeConstants } from 'plugins/kibana/visualize/visualize_constants';
 
-function visualizeReportProvider(Private) {
+function visualizeReportProvider(Private, $location) {
   const xpackInfo = Private(XPackInfoProvider);
   return {
     appName: 'visualize',
@@ -11,7 +12,10 @@ function visualizeReportProvider(Private) {
     label: 'Reporting',
     template: '<export-config object-type="Visualization"></export-config>',
     description: 'Visualization Report',
-    hideButton: () => !xpackInfo.get('features.reporting.showLinks', false),
+    hideButton: () => (
+      $location.path() === VisualizeConstants.LANDING_PAGE_URL
+      || !xpackInfo.get('features.reporting.showLinks', false)
+    ),
     disableButton: () => !xpackInfo.get('features.reporting.enableLinks', false),
     tooltip: () => xpackInfo.get('features.reporting.message')
   };
