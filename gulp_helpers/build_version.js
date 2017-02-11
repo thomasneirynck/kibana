752ed43fda4fcca71ffe06c0ca7914d245f5c00d
Court Ewing <court@epixa.com>
2017-02-11 07:54:41 -0500

    build: get version from kibana package.json (#427)
    With the upcoming X-Pack split, the x-pack-kibana build process needs to
    track its own version rather than relying on the version from
    Elasticsearch.
    
    The old version argument is no longer relevant either, so it is removed.

--- gulp_helpers/build_version.js
+++ gulp_helpers/build_version.js
@@ -1,48 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-const properties = require('properties');
 const yargs = require('yargs');
 const semver = require('semver');
 const pkg = require('../package.json');
 
 yargs
-.alias('r', 'release').describe('r', 'Create a release build, not a snapshot')
-.alias('v', 'version').describe('v', 'Explicitely set the version')
-.describe('fallback', 'Fall back to the version in the package.json file');
+.alias('r', 'release').describe('r', 'Create a release build, not a snapshot');
 const argv = yargs.argv;
 
-const propFile = path.resolve('..', 'elasticsearch', 'buildSrc', 'version.properties');
-
-function getFileVersion() {
-  const snapshotText = (argv.release) ? '' : '-SNAPSHOT';
-
-  try {
-    // throws if file can not be read or found
-    fs.accessSync(propFile, fs.R_OK);
-    const contents = fs.readFileSync(propFile, { encoding: 'utf8' });
-    const props = properties.parse(contents);
-    return props.elasticsearch + snapshotText;
-  } catch (e) {
-    if (!argv.fallback) {
-      throw new Error('Could not read version from ' + propFile + ', try using --fallback');
-    }
-    return pkg.version.replace(/\-snapshot/i, snapshotText);
-  }
-}
-
 function getVersion() {
-  const fileVersion = getFileVersion();
-  // ensure valid version
-  if (!argv.version) return fileVersion;
-  if (!semver.valid(argv.version)) throw new Error('Version is invalid: ' + argv.version);
+  const { version } = pkg;
+  if (!version) {
+    throw new Error('No version found in package.json');
+  }
+  if (!semver.valid(version)) {
+    throw new Error(`Version is not valid semver: ${version}`);
+  }
 
-  // ensure version roughly matches
-  const minVer = '^' + fileVersion.split('-')[0];
-  const compareVer = argv.version.split('-')[0];
-
-  if (!semver.satisfies(compareVer, minVer)) throw new Error('Version does not match ' + minVer);
-
-  return argv.version;
+  const snapshotText = (argv.release) ? '' : '-SNAPSHOT';
+  return `${version}${snapshotText}`;
 }
 
 module.exports = getVersion;
