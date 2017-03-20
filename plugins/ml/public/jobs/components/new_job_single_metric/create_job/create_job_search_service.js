thomasneirynck/kibana

commit 6f1614f27c4898d3d845bf571023691e4e75151b

[ML] rename model debug to model plot (#627)
Downstream changes after renaming model debug to model plot

Changes ML calls according to upstream changes, see x-pack-elasticsearch#284 for details.

plugins/ml/public/jobs/components/new_job_single_metric/create_job/create_job_search_service.js
@@ -119,7 +119,7 @@ module.service('mlSingleMetricJobSearchService', function ($q, es) {
 
 
 
-  this.getModelDebugOutput = function (index, jobIds, earliestMs, latestMs, interval, aggType) {
+  this.getModelPlotOutput = function (index, jobIds, earliestMs, latestMs, interval, aggType) {
     const deferred = $q.defer();
     const obj = {
       success: true,
@@ -169,7 +169,7 @@ module.service('mlSingleMetricJobSearchService', function ($q, es) {
           'bool': {
             'filter': [{
               'query_string': {
-                'query': '_type:result AND result_type:model_debug_output',
+                'query': '_type:result AND result_type:model_plot_output',
                 'analyze_wildcard': true
               }
             }, {
@@ -192,16 +192,16 @@ module.service('mlSingleMetricJobSearchService', function ($q, es) {
                   'field': 'actual'
                 }
               },
-              'debugUpper': {
+              'modelUpper': {
                 // 'max': {
                 [aggType.max]: {
-                  'field': 'debug_upper'
+                  'field': 'model_upper'
                 }
               },
-              'debugLower': {
+              'modelLower': {
                 // 'min': {
                 [aggType.min]: {
-                  'field': 'debug_lower'
+                  'field': 'model_lower'
                 }
               }
             }
@@ -210,29 +210,29 @@ module.service('mlSingleMetricJobSearchService', function ($q, es) {
       }
     })
     .then((resp) => {
-      // console.log('Time series search service getModelDebugOutput() resp:', resp);
+      // console.log('Time series search service getModelPlotOutput() resp:', resp);
 
       const aggregationsByTime = _.get(resp, ['aggregations', 'times', 'buckets'], []);
       _.each(aggregationsByTime, (dataForTime) => {
         const time = dataForTime.key;
-        let debugUpper = _.get(dataForTime, ['debugUpper', 'value']);
-        let debugLower = _.get(dataForTime, ['debugLower', 'value']);
+        let modelUpper = _.get(dataForTime, ['modelUpper', 'value']);
+        let modelLower = _.get(dataForTime, ['modelLower', 'value']);
 
-        if (debugUpper !== undefined && isFinite(debugUpper)) {
-          debugUpper = debugUpper.toFixed(4);
+        if (modelUpper !== undefined && isFinite(modelUpper)) {
+          modelUpper = modelUpper.toFixed(4);
         } else {
-          debugUpper = 0;
+          modelUpper = 0;
         }
-        if (debugLower !== undefined && isFinite(debugLower)) {
-          debugLower = debugLower.toFixed(4);
+        if (modelLower !== undefined && isFinite(modelLower)) {
+          modelLower = modelLower.toFixed(4);
         } else {
-          debugLower = 0;
+          modelLower = 0;
         }
 
         obj.results[time] = {
           actual: _.get(dataForTime, ['actual', 'value']),
-          debugUpper: debugUpper,
-          debugLower: debugLower
+          modelUpper: modelUpper,
+          modelLower: modelLower
         };
       });
 

plugins/ml/public/timeseriesexplorer/timeseries_search_service.js
@@ -20,7 +20,7 @@ const module = uiModules.get('apps/ml');
 
 module.service('mlTimeSeriesSearchService', function ($q, $timeout, es) {
 
-  this.getModelDebugOutput = function (index, jobIds, earliestMs, latestMs, interval) {
+  this.getModelPlotOutput = function (index, jobIds, earliestMs, latestMs, interval) {
     const deferred = $q.defer();
     const obj = {
       success: true,
@@ -65,7 +65,7 @@ module.service('mlTimeSeriesSearchService', function ($q, $timeout, es) {
           'bool': {
             'filter': [{
               'query_string': {
-                'query': '_type:result AND result_type:model_debug_output',
+                'query': '_type:result AND result_type:model_plot_output',
                 'analyze_wildcard': true
               }
             }, {
@@ -88,14 +88,14 @@ module.service('mlTimeSeriesSearchService', function ($q, $timeout, es) {
                   'field': 'actual'
                 }
               },
-              'debugUpper': {
+              'modelUpper': {
                 'max': {
-                  'field': 'debug_upper'
+                  'field': 'model_upper'
                 }
               },
-              'debugLower': {
+              'modelLower': {
                 'min': {
-                  'field': 'debug_lower'
+                  'field': 'model_lower'
                 }
               }
             }
@@ -104,15 +104,15 @@ module.service('mlTimeSeriesSearchService', function ($q, $timeout, es) {
       }
     })
     .then(function (resp) {
-      console.log('Time series search service getModelDebugOutput() resp:', resp);
+      console.log('Time series search service getModelPlotOutput() resp:', resp);
 
       const aggregationsByTime = _.get(resp, ['aggregations', 'times', 'buckets'], []);
       _.each(aggregationsByTime, function (dataForTime) {
         const time = dataForTime.key;
         obj.results[time] = {
           'actual': _.get(dataForTime, ['actual', 'value']),
-          'debugUpper': _.get(dataForTime, ['debugUpper', 'value']),
-          'debugLower': _.get(dataForTime, ['debugLower', 'value'])
+          'modelUpper': _.get(dataForTime, ['modelUpper', 'value']),
+          'modelLower': _.get(dataForTime, ['modelLower', 'value'])
         };
       });
 

plugins/ml/lib/initialization_checks.js
@@ -181,13 +181,13 @@ module.exports = function (plugin, server) {
             doc_values: true, searchable: true, aggregatable: true },
           { name: 'actual', type: 'number', count: 0, scripted: false, indexed: true, analyzed: false,
             doc_values: true, searchable: true, aggregatable: true },
-          { name: 'debug_upper', type: 'number', count: 0, scripted: false, indexed: true, analyzed: false,
+          { name: 'model_upper', type: 'number', count: 0, scripted: false, indexed: true, analyzed: false,
             doc_values: true, searchable: true, aggregatable: true },
-          { name: 'debug_lower', type: 'number', count: 0, scripted: false, indexed: true, analyzed: false,
+          { name: 'model_lower', type: 'number', count: 0, scripted: false, indexed: true, analyzed: false,
             doc_values: true, searchable: true, aggregatable: true },
-          { name: 'debug_median', type: 'number', count: 0, scripted: false, indexed: true, analyzed: false,
+          { name: 'model_median', type: 'number', count: 0, scripted: false, indexed: true, analyzed: false,
             doc_values: true, searchable: true, aggregatable: true },
-          { name: 'debug_feature', type: 'string', count: 0, scripted: false, indexed: true, analyzed: false,
+          { name: 'model_feature', type: 'string', count: 0, scripted: false, indexed: true, analyzed: false,
             doc_values: true, searchable: true, aggregatable: true },
           { name: 'causes.function', type: 'string', count: 0, scripted: false, indexed: true, analyzed: false,
             doc_values: true, searchable: true, aggregatable: true },

plugins/ml/public/jobs/components/new_job_multi_metric/create_job/create_job_chart_directive.js
@@ -14,7 +14,7 @@
  */
 
 /*
- * Chart showing model debug data, annotated with anomalies.
+ * Chart showing model plot data, annotated with anomalies.
  */
 
 import $ from 'jquery';

plugins/ml/public/jobs/components/new_job_multi_metric/create_job/create_job_controller.js
@@ -253,7 +253,7 @@ module
   }
 
   // ensure the displayed interval is never smaller than the bucketSpan
-  // otherwise the model debug bounds can be drawn in the wrong place.
+  // otherwise the model plot bounds can be drawn in the wrong place.
   // this only really affects small jobs when using sum
   function adjustIntervalDisplayed(interval) {
     let makeTheSame = false;

plugins/ml/public/jobs/components/new_job_multi_metric/create_job/create_job_eventrate_directive.js
@@ -14,7 +14,7 @@
  */
 
 /*
- * Chart showing model debug data, annotated with anomalies.
+ * Chart showing model plot data, annotated with anomalies.
  */
 
 import $ from 'jquery';

plugins/ml/public/jobs/components/new_job_multi_metric/create_job/filter_agg_types.js
@@ -21,7 +21,7 @@ function filterAggTypes(aggTypes) {
   let typeCopy;
   _.each(aggTypes, (type) => {
     type.mlName = type.name;
-    type.mlDebugAgg = {max:type.name, min: type.name};
+    type.mlModelPlotAgg = {max:type.name, min: type.name};
 
     _.each(type.params, (p) => {
       if (p.filterFieldTypes && typeof p.filterFieldTypes === 'string') {
@@ -30,19 +30,19 @@ function filterAggTypes(aggTypes) {
     });
 
     if (type.name === 'count') {
-      type.mlDebugAgg = {max: 'max', min: 'min'};
+      type.mlModelPlotAgg = {max: 'max', min: 'min'};
       filteredAggTypes.push(type);
 
       typeCopy = angular.copy(type);
       typeCopy.mlName = 'high_count';
       typeCopy.title   = 'High count';
-      typeCopy.mlDebugAgg = {max: 'max', min: 'min'};
+      typeCopy.mlModelPlotAgg = {max: 'max', min: 'min'};
       filteredAggTypes.push(typeCopy);
 
       typeCopy = angular.copy(type);
       typeCopy.mlName = 'low_count';
       typeCopy.title   = 'Low count';
-      typeCopy.mlDebugAgg = {max: 'max', min: 'min'};
+      typeCopy.mlModelPlotAgg = {max: 'max', min: 'min'};
       filteredAggTypes.push(typeCopy);
 
     } else if (type.name === 'sum') {
@@ -77,7 +77,7 @@ function filterAggTypes(aggTypes) {
     } else if (type.name === 'max') {
       filteredAggTypes.push(type);
     } else if (type.name === 'cardinality') {
-      type.mlDebugAgg = {max: 'max', min: 'min'};
+      type.mlModelPlotAgg = {max: 'max', min: 'min'};
       type.mlName = 'distinct_count';
 
       _.each(type.params, (p) => {

plugins/ml/public/jobs/components/new_job_single_metric/create_job/create_job_chart_directive.js
@@ -14,7 +14,7 @@
  */
 
 /*
- * Chart showing model debug data, annotated with anomalies.
+ * Chart showing model plot data, annotated with anomalies.
  */
 
 import $ from 'jquery';

commit a225c9e72b57e5a4aba3b0585fc8e7a4e6dc35ac

fix a regression caused by model_debug rename (#652)
Fixes a late time rename of model_plot_output to model_plot

Regression introduced in #627

plugins/ml/public/jobs/components/new_job_single_metric/create_job/create_job_search_service.js
@@ -169,7 +169,7 @@ module.service('mlSingleMetricJobSearchService', function ($q, es) {
           'bool': {
             'filter': [{
               'query_string': {
-                'query': '_type:result AND result_type:model_plot_output',
+                'query': '_type:result AND result_type:model_plot',
                 'analyze_wildcard': true
               }
             }, {

plugins/ml/public/timeseriesexplorer/timeseries_search_service.js
@@ -65,7 +65,7 @@ module.service('mlTimeSeriesSearchService', function ($q, $timeout, es) {
           'bool': {
             'filter': [{
               'query_string': {
-                'query': '_type:result AND result_type:model_plot_output',
+                'query': '_type:result AND result_type:model_plot',
                 'analyze_wildcard': true
               }
             }, {