222ce27c7f98b811e6cc9dd9431ac4de5288136d
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-01 16:54:28 -0700

    Handle errors on action acknowledgement (#1125)

--- plugins/watcher/server/routes/api/watch/action/register_acknowledge_route.js
+++ plugins/watcher/server/routes/api/watch/action/register_acknowledge_route.js
@@ -1,9 +1,13 @@
 import { get } from 'lodash';
 import { callWithRequestFactory } from '../../../../lib/call_with_request_factory';
+import { isEsErrorFactory } from '../../../../lib/is_es_error_factory';
+import { wrapEsError, wrapUnknownError } from '../../../../lib/error_wrappers';
 import { WatchStatus } from '../../../../models/watch_status';
 import { licensePreRoutingFactory } from'../../../../lib/license_pre_routing_factory';
 
 export function registerAcknowledgeRoute(server) {
+
+  const isEsError = isEsErrorFactory(server);
   const licensePreRouting = licensePreRoutingFactory(server);
 
   server.route({
@@ -23,6 +27,19 @@ export function registerAcknowledgeRoute(server) {
 
         const watchStatus = WatchStatus.fromUpstreamJSON(json);
         reply({ watchStatus: watchStatus.downstreamJSON });
+      })
+      .catch(err => {
+
+        // Case: Error from Elasticsearch JS client
+        if (isEsError(err)) {
+          const statusCodeToMessageMap = {
+            404: `Watch with id = ${watchId} not found`
+          };
+          return reply(wrapEsError(err, statusCodeToMessageMap));
+        }
+
+        // Case: default
+        reply(wrapUnknownError(err));
       });
     },
     config: {
