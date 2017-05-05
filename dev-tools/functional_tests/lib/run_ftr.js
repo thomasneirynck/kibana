85a2bb752ce5c3dca6f5216784f3aaadfac7f291
spalger <spalger@users.noreply.github.com>
2017-05-05 12:13:15 -0700

    [dev-tools/functional_tests] run ftr in debug mode

--- dev-tools/functional_tests/lib/run_ftr.js
+++ dev-tools/functional_tests/lib/run_ftr.js
@@ -7,7 +7,7 @@ import {
 export async function runFtr({ procs }) {
   await procs.run('ftr', {
     cmd: 'node',
-    args: [KIBANA_FTR_SCRIPT],
+    args: [KIBANA_FTR_SCRIPT, '--debug'],
     cwd: PROJECT_ROOT,
     wait: true
   });
