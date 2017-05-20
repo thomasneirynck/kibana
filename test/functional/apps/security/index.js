0d21f1b55cfa2caafb87662fd66c49943e554dcf
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2017-05-19 17:08:26 -0700

    Converting a test from integration test repo to x-pack-kibana format.  (#1293)
    * converting one user test from integration test repo to x-pack-kibana repo
    
    * renamed the file
    
    * fixed indentation

--- test/functional/apps/security/index.js
+++ test/functional/apps/security/index.js
@@ -2,5 +2,6 @@ export default function ({ loadTestFile }) {
   describe('security app', function () {
     loadTestFile(require.resolve('./security'));
     loadTestFile(require.resolve('./management'));
+    loadTestFile(require.resolve('./users'));
   });
 }
