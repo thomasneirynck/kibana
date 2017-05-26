c10e04a94fea0059e96d190726bd339973ea8c1c
Jim Unger <jim.unger@elastic.co>
2017-05-26 13:29:29 -0500

    removing testbed code (#1383)

--- plugins/watcher/public/sections/testbed/components/testbed/testbed.js
+++ plugins/watcher/public/sections/testbed/components/testbed/testbed.js
@@ -1,32 +1,20 @@
 import { uiModules } from 'ui/modules';
 import template from './testbed.html';
 import './testbed.less';
-import 'plugins/watcher/services/fields';
 
 const app = uiModules.get('xpack/watcher');
 
-app.directive('testbed', function ($injector) {
-  const service = $injector.get('watcherFieldsService');
+app.directive('testbed', function () {
 
   return {
     restrict: 'E',
     template: template,
     scope: {
-      data: '='
     },
     bindToController: true,
     controllerAs: 'testbed',
     controller: class TestbedController {
       constructor() {
-      }
-
-      loadFields = () => {
-        this.fields = undefined;
-        const indexes = this.indexPatterns ? this.indexPatterns.split(',') : [];
-        service.getFields(indexes)
-        .then((fields) => {
-          this.fields = fields;
-        });
       }
     }
   };
