756d24783e710e7937ac265841cdada58e84b056
Shaunak Kashyap <ycombinator@gmail.com>
2017-08-08 06:28:27 -0700

    Making unit tests pass (#2220)

--- plugins/ml/public/filters/__tests__/metric_change_description.js
+++ plugins/ml/public/filters/__tests__/metric_change_description.js
@@ -39,21 +39,21 @@ describe('ML - metricChangeDescription filter', function () {
   });
 
   it('returns correct description if actual > typical', () => {
-    expect(filter(1.01, 1)).to.be('<i class="fa fa-arrow-up"></i> Unusually high');
-    expect(filter(1.123, 1)).to.be('<i class="fa fa-arrow-up"></i> 1.1x higher');
-    expect(filter(2, 1)).to.be('<i class="fa fa-arrow-up"></i> 2x higher');
-    expect(filter(9.5, 1)).to.be('<i class="fa fa-arrow-up"></i> 10x higher');
-    expect(filter(1000, 1)).to.be('<i class="fa fa-arrow-up"></i> More than 100x higher');
-    expect(filter(1, 0)).to.be('<i class="fa fa-arrow-up"></i> Unexpected non-zero value');
+    expect(filter(1.01, 1)).to.be('<i class="fa fa-arrow-up" aria-hidden="true"></i> Unusually high');
+    expect(filter(1.123, 1)).to.be('<i class="fa fa-arrow-up" aria-hidden="true"></i> 1.1x higher');
+    expect(filter(2, 1)).to.be('<i class="fa fa-arrow-up" aria-hidden="true"></i> 2x higher');
+    expect(filter(9.5, 1)).to.be('<i class="fa fa-arrow-up" aria-hidden="true"></i> 10x higher');
+    expect(filter(1000, 1)).to.be('<i class="fa fa-arrow-up" aria-hidden="true"></i> More than 100x higher');
+    expect(filter(1, 0)).to.be('<i class="fa fa-arrow-up" aria-hidden="true"></i> Unexpected non-zero value');
   });
 
   it('returns correct description if actual < typical', () => {
-    expect(filter(1, 1.01)).to.be('<i class="fa fa-arrow-down"></i> Unusually low');
-    expect(filter(1, 1.123)).to.be('<i class="fa fa-arrow-down"></i> 1.1x lower');
-    expect(filter(1, 2)).to.be('<i class="fa fa-arrow-down"></i> 2x lower');
-    expect(filter(1, 9.5)).to.be('<i class="fa fa-arrow-down"></i> 10x lower');
-    expect(filter(1, 1000)).to.be('<i class="fa fa-arrow-down"></i> More than 100x lower');
-    expect(filter(0, 1)).to.be('<i class="fa fa-arrow-down"></i> Unexpected zero value');
+    expect(filter(1, 1.01)).to.be('<i class="fa fa-arrow-down" aria-hidden="true"></i> Unusually low');
+    expect(filter(1, 1.123)).to.be('<i class="fa fa-arrow-down" aria-hidden="true"></i> 1.1x lower');
+    expect(filter(1, 2)).to.be('<i class="fa fa-arrow-down" aria-hidden="true"></i> 2x lower');
+    expect(filter(1, 9.5)).to.be('<i class="fa fa-arrow-down" aria-hidden="true"></i> 10x lower');
+    expect(filter(1, 1000)).to.be('<i class="fa fa-arrow-down" aria-hidden="true"></i> More than 100x lower');
+    expect(filter(0, 1)).to.be('<i class="fa fa-arrow-down" aria-hidden="true"></i> Unexpected zero value');
   });
 
 });
