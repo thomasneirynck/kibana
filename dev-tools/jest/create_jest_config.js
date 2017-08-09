c2e08b866cfc36fd150edfdb4e36ec000eec302b
Søren Louv-Jansen <sorenlouv@gmail.com>
2017-08-09 19:09:32 +0200

    [Jest] Mock css files (#2178)

--- dev-tools/jest/create_jest_config.js
+++ dev-tools/jest/create_jest_config.js
@@ -13,6 +13,8 @@ export function createJestConfig({
     ],
     moduleNameMapper: {
       "^ui_framework/components": `${kibanaDirectory}/ui_framework/components`,
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":`${kibanaDirectory}/src/jest/file_mock.js`,
+      "\\.(css|less|scss)$": `${kibanaDirectory}/src/jest/style_mock.js`
     },
     testMatch: [
       "**/*.test.js"
