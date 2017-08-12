b5fa89654fadb1dc55b224ef8a3f15ceacbf9c0c
Tim Sullivan <tsullivan@users.noreply.github.com>
2017-08-11 17:37:45 -0700

    [Monitoring] use xpackInfo off the admin cluster to determine if Kibana Monitoring should be enabled (#2263)
    * [Monitoring] use xpackInfo off the admin cluster to determine if Kibana Monitoring should be enabled
    
    * expound on error log message

--- plugins/monitoring/server/kibana_monitoring/init_kibana_monitoring.js
+++ plugins/monitoring/server/kibana_monitoring/init_kibana_monitoring.js
@@ -1,4 +1,3 @@
-import { get } from 'lodash';
 import { LOGGING_TAG, KIBANA_MONITORING_LOGGING_TAG, } from '../../common/constants';
 import { monitoringBulk } from './lib/monitoring_bulk';
 import { startCollector } from './start_collector';
@@ -8,27 +7,19 @@ import { startCollector } from './start_collector';
  * @param server {Object} HapiJS server instance
  */
 export async function initKibanaMonitoring(kbnServer, server) {
-  const { callWithInternalUser } = server.plugins.elasticsearch.getCluster('admin');
+  const mainXpackInfo = server.plugins.xpack_main.info;
+  const mainMonitoring = mainXpackInfo.feature('monitoring');
 
-  let xpackInfo;
-  try {
-    xpackInfo = await callWithInternalUser('transport.request', {
-      method: 'GET',
-      path: '/_xpack'
-    });
-  } catch(err) {
-    server.log(
-      ['error', LOGGING_TAG, KIBANA_MONITORING_LOGGING_TAG],
-      'Unable to retrieve X-Pack info. Kibana monitoring will be disabled.'
-    );
-  }
-
-  const isEnabledInAdminCluster = get(xpackInfo, 'features.monitoring.enabled', false);
-  if (isEnabledInAdminCluster) {
+  if (mainXpackInfo && mainMonitoring.isAvailable() && mainMonitoring.isEnabled()) {
     const client = server.plugins.elasticsearch.getCluster('admin').createClient({
       plugins: [monitoringBulk]
     });
 
     startCollector(kbnServer, server, client);
+  } else {
+    server.log(
+      ['error', LOGGING_TAG, KIBANA_MONITORING_LOGGING_TAG],
+      'Unable to retrieve X-Pack info from the admin cluster. Kibana monitoring will be disabled until Kibana is restarted.'
+    );
   }
 }
