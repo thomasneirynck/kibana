f6ceb6c1fc6bb16c773b328fedefc9af38c33c0f
Chris Cowan <chris@chriscowan.us>
2017-08-14 13:21:23 -0700

    Fixes #2276 - Default time picker to last 24 hours (#2277)

--- plugins/apm/public/utils/timepicker/index.js
+++ plugins/apm/public/utils/timepicker/index.js
@@ -8,10 +8,22 @@ import moment from 'moment';
 import { uiModules } from 'ui/modules';
 import 'ui/chrome';
 import 'ui/autoload/all';
+import { set } from 'lodash';
 
 let globalTimefilter;
 
 export function initTimepicker(callback) {
+  uiModules.get('kibana').run(function(uiSettings) {
+    set(
+      uiSettings,
+      'defaults.timepicker:timeDefaults.value',
+      JSON.stringify({
+        from: 'now-24h',
+        to: 'now',
+        mode: 'quick'
+      })
+    );
+  });
   uiModules
     .get('app/apm', [])
     .controller('TimePickerController', ($scope, timefilter) => {
