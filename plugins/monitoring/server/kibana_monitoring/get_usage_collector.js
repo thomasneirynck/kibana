4dab43762428458974b28746c62c03ce9df9171d
Spencer <spalger@users.noreply.github.com>
2017-08-16 13:11:52 -0700

    [monitoring/kibana/usageCollector] use server.getKibanaStats() api (#2234)

--- plugins/monitoring/server/kibana_monitoring/get_usage_collector.js
+++ plugins/monitoring/server/kibana_monitoring/get_usage_collector.js
@@ -1,8 +1,6 @@
 import { KIBANA_USAGE_TYPE } from '../../common/constants';
 import { getAuthHeader } from '../../../security/server/lib/basic_auth';
 
-const STATS_ENDPOINT = '/api/stats';
-
 /*
  * Use HapiJS server.inject to call the Kibana stats API
  * Note: `inject` simulates an API call without the overhead of a network
@@ -11,23 +9,17 @@ const STATS_ENDPOINT = '/api/stats';
 export function getUsageCollector(server, config) {
   const username = config.get('elasticsearch.username');
   const password = config.get('elasticsearch.password');
-  const injectOptions = {
-    method: 'GET',
-    url: STATS_ENDPOINT,
+  const fakeRequest = {
     headers: getAuthHeader(username, password)
   };
+
+  const { callWithRequest } = server.plugins.elasticsearch.getCluster('admin');
+  const callCluster = (...args) => callWithRequest(fakeRequest, ...args);
 
   return {
     type: KIBANA_USAGE_TYPE,
     fetch() {
-      return server.inject(injectOptions)
-      .then(({ statusCode, result }) => {
-        if (statusCode === 200) {
-          return result;
-        }
-        // unexpected response - log the status code
-        throw new Error(`Kibana Stats API call failed! Status code: ${statusCode}`);
-      });
+      return server.getKibanaStats({ callCluster });
     }
   };
 };
