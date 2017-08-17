2e369f93c3f4fc5715d3cfe376aada411cd52506
Tim Sullivan <tsullivan@users.noreply.github.com>
2017-08-17 10:20:09 -0700

    [Monitoring] Small cleanup for functional tests (#2311)
    Making this change in a standalone PR to ensure that removing this method doesn’t break the build

--- test/functional/page_objects/monitoring_page.js
+++ test/functional/page_objects/monitoring_page.js
@@ -1,30 +1,14 @@
-export function MonitoringPageProvider({ getService, getPageObjects }) {
-  const remote = getService('remote');
-  const log = getService('log');
-  const kibanaServer = getService('kibanaServer');
-  const esArchiver = getService('esArchiver');
+export function MonitoringPageProvider({ getPageObjects, getService }) {
   const PageObjects = getPageObjects(['common', 'header']);
   const testSubjects = getService('testSubjects');
 
-  class MonitoringPage {
-    async initTests() {
-      await kibanaServer.uiSettings.disableToastAutohide();
-      log.debug('MonitoringPage:initTests');
-      await esArchiver.load('empty_kibana');
-      await esArchiver.loadIfNeeded('logstash_functional');
-      remote.setWindowSize(1600,1000);
-    }
-
-
+  return new class MonitoringPage {
     async navigateTo() {
       await PageObjects.common.navigateToApp('monitoring');
-
     }
-
 
     async getAccessDeniedMessage() {
       return testSubjects.getVisibleText('accessDeniedTitle');
     }
-}
-  return new MonitoringPage();
+  };
 }
