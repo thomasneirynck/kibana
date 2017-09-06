683211f4251f4ad21fd0c1456f215c45d6b82a71
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2017-09-06 09:46:14 -0700

    Conversion of Doc level security test from Integration repo to x-pack-kibana (#2338)
    * securitytestconversion
    
    * es_archiver data for DLStest
    
    * doc level security test
    
    * made changes for the  dlsuser to logout in the test
    
    * incorporated the changes for createIndexPatternmethod()
    
    * index.js file incorporating the required file
    
    *  remove unwanted file from index.js

--- test/functional/apps/security/index.js
+++ test/functional/apps/security/index.js
@@ -1,6 +1,7 @@
 export default function ({ loadTestFile }) {
   describe('security app', function () {
     loadTestFile(require.resolve('./security'));
+    loadTestFile(require.resolve('./doc_level_security_roles'));
     loadTestFile(require.resolve('./management'));
     loadTestFile(require.resolve('./users'));
     loadTestFile(require.resolve('./secure_roles_perm'));
