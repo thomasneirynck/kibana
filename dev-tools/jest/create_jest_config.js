d8a3059247518f09e74db43323b71157e608b44b
Søren Louv-Jansen <sorenlouv@gmail.com>
2017-10-03 13:19:40 +0200

    [Jest] Fix style_mock location (#2749)

--- dev-tools/jest/create_jest_config.js
+++ dev-tools/jest/create_jest_config.js
@@ -14,7 +14,7 @@ export function createJestConfig({
     moduleNameMapper: {
       "^ui_framework/components": `${kibanaDirectory}/ui_framework/components`,
       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":`${kibanaDirectory}/src/jest/file_mock.js`,
-      "\\.(css|less|scss)$": `${kibanaDirectory}/src/jest/style_mock.js`
+      "\\.(css|less|scss)$": `${kibanaDirectory}/src/jest/mocks/style_mock.js`
     },
     testMatch: [
       "**/*.test.js"
