thomasneirynck/kibana

commit f4e60445d8ecd5cdd47aa9e5349219f527c9b658

[ML] Fixes form info tooltip by appending it to body. (#3984)
The form info tooltip was rendered inside the application's div and could end up partially hidden behind the main menu bar to the left. This fixes it by appending the tooltip to the body tag which makes use of its z-index correctly.

plugins/ml/public/components/json_tooltip/json_tooltip.js
@@ -44,7 +44,7 @@ module.service('mlJsonTooltipService', function () {
       restrict: 'AE',
       replace: true,
       template: `
-      <i aria-hidden="true" class="fa fa-info-circle" tooltip="{{text}}">
+      <i aria-hidden="true" class="fa fa-info-circle" tooltip="{{text}}" tooltip-append-to-body="1">
         <span id="ml_aria_description_{{id}}" class="ml-info-tooltip-text">{{text}}</span>
       </i>
     `,

commit 0344bafdf4196bd658e071ec74b0b77d9e951dcb

[ML] Fixes an issue with tooltips not showing up in modals. (#4082)
Fixes a regression bug introduced by #3984 which caused tooltips inside modals being displayed under the modal. The ml-form-label directive now supports the tooltip-append-to-body attribute which gets piped through to the element with the tooltip. It defaults to false/0 so the tooltip will get added inline. Using for example <ml-form-label label-id="new_job_id" tooltip-append-to-body="true">Name</ml-form-label> appends the tooltip to the body which again solves the initial issue #3984 where tooltips ended up under the Kibana sidebar. I went through all job wizards and added tooltip-append-to-body="true" to info-icons where the label is to close to the left side.

plugins/ml/public/components/form_label/form_label.js
@@ -31,13 +31,14 @@ module.directive('mlFormLabel', function () {
   return {
     scope: {
       labelId: '@',
+      tooltipAppendToBody: '@'
     },
     restrict: 'E',
     replace: false,
     transclude: true,
     template: `
       <label class="kuiFormLabel" id="ml_aria_label_{{labelId}}" ng-transclude></label>
-      <i ml-info-icon="{{labelId}}" />
+      <i ml-info-icon="{{labelId}}" tooltip-append-to-body="{{tooltipAppendToBody}}" />
     `
   };
 });

plugins/ml/public/components/json_tooltip/json_tooltip.js
@@ -39,12 +39,12 @@ module.service('mlJsonTooltipService', function () {
   .directive('mlInfoIcon', function () {
     return {
       scope: {
-        id: '@mlInfoIcon',
+        id: '@mlInfoIcon'
       },
       restrict: 'AE',
       replace: true,
       template: `
-      <i aria-hidden="true" class="fa fa-info-circle" tooltip="{{text}}" tooltip-append-to-body="1">
+      <i aria-hidden="true" class="fa fa-info-circle" tooltip="{{text}}">
         <span id="ml_aria_description_{{id}}" class="ml-info-tooltip-text">{{text}}</span>
       </i>
     `,

plugins/ml/public/jobs/new_job/advanced/new_job.html
@@ -81,7 +81,7 @@
           <div class="tab_contents">
             <!-- ID -->
             <div class="form-group">
-              <ml-form-label label-id="new_job_id">Name</ml-form-label>
+              <ml-form-label label-id="new_job_id" tooltip-append-to-body="true">Name</ml-form-label>
               <input
                 aria-labelledby="ml_aria_label_new_job_id"
                 aria-describedby="ml_aria_description_new_job_id"
@@ -432,7 +432,7 @@
 
             <div ng-if="ui.isDatafeed">
               <div class="form-group">
-                <ml-form-label label-id="new_job_datafeed_query">Query</ml-form-label>
+                <ml-form-label label-id="new_job_datafeed_query" tooltip-append-to-body="true">Query</ml-form-label>
                 <input
                   aria-labelledby="ml_aria_label_new_job_datafeed_query"
                   aria-describedby="ml_aria_description_new_job_datafeed_query"
@@ -464,7 +464,7 @@
 
               </div>
               <div class="form-group" >
-                <ml-form-label label-id="new_job_datafeed_scrollsize">scroll_size</ml-form-label>
+                <ml-form-label label-id="new_job_datafeed_scrollsize" tooltip-append-to-body="true">scroll_size</ml-form-label>
                 <input
                   aria-labelledby="ml_aria_label_new_job_datafeed_scrollsize"
                   aria-describedby="ml_aria_description_new_job_datafeed_scrollsize"

plugins/ml/public/jobs/new_job/simple/components/general_job_details/general_job_details.html
@@ -1,6 +1,6 @@
 <div class="general-job-details">
   <div class="form-group">
-    <ml-form-label label-id="new_job_id">Name</ml-form-label>
+    <ml-form-label label-id="new_job_id" tooltip-append-to-body="true">Name</ml-form-label>
     <input
       aria-labelledby="ml_aria_label_new_job_id"
       aria-describedby="ml_aria_description_new_job_id"

plugins/ml/public/jobs/new_job/simple/single_metric/create_job/create_job.html
@@ -46,7 +46,7 @@
       </div>
 
       <div class="form-group col-md-4">
-        <ml-form-label label-id="new_job_detector_fieldname_subset">Field</ml-form-label>
+        <ml-form-label label-id="new_job_detector_fieldname_subset" tooltip-append-to-body="true">Field</ml-form-label>
         <ui-select
           aria-labelledby="ml_aria_label_new_job_detector_fieldname_subset"
           aria-describedby="ml_aria_description_new_job_detector_fieldname_subset"
@@ -141,7 +141,7 @@
 
     <div ng-show="ui.showJobInput && (jobState === JOB_STATE.NOT_STARTED || jobState === JOB_STATE.RUNNING || jobState === JOB_STATE.STOPPING)">
       <div class="form-group">
-        <ml-form-label label-id="new_job_id">Name</ml-form-label>
+        <ml-form-label label-id="new_job_id" tooltip-append-to-body="true">Name</ml-form-label>
         <input
           aria-labelledby="ml_aria_label_new_job_id"
           aria-describedby="ml_aria_description_new_job_id"