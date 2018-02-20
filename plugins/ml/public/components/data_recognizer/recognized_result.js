68cfd570a979e66703c55a312f7a8080eb598357
James Gowdy <jgowdy@elastic.co>
2018-02-20 16:44:21 +0000

    [ML] Job type selector icon style tweak (#4665)

--- plugins/ml/public/components/data_recognizer/recognized_result.js
+++ plugins/ml/public/components/data_recognizer/recognized_result.js
@@ -25,7 +25,7 @@ export const RecognizedResult = ({
   // if a logo is available, use that, otherwise display the id
   // the logo should be a base64 encoded image
   if (config.logo && config.logo.src) {
-    logo = <div><img src={config.logo.src}/></div>;
+    logo = <div><img className="synopsisIcon" src={config.logo.src}/></div>;
   } else {
     logo = <h3 className="euiTitle euiTitle--small">{config.id}</h3>;
   }
