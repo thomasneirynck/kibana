386baa1dbc6efe075964e9a7f234fa4f9882b18a
Justin Kambic <justin.kambic@elastic.co>
2018-03-15 14:50:56 -0400

    [Dev Tools] Update path to file_mock.js in OSS Kibana (#4800)
    * Updated jest config to reference current OSS Kibana directory for mocks files.

--- dev-tools/jest/create_jest_config.js
+++ dev-tools/jest/create_jest_config.js
@@ -12,7 +12,8 @@ export function createJestConfig({
       "json"
     ],
     moduleNameMapper: {
-      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `${kibanaDirectory}/src/dev/jest/file_mock.js`,
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        `${kibanaDirectory}/src/dev/jest/mocks/file_mock.js`,
       "\\.(css|less|scss)$": `${kibanaDirectory}/src/dev/jest/mocks/style_mock.js`
     },
     setupFiles: [
